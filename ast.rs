6ae42470cf6c5c03b91ca4d5cce73ec35757f6d3
Markus <marwes91@gmail.com>
2014-08-01 23:26:02 +0200

    Added parsing of operators

--- ast.rs
+++ ast.rs
@@ -1,45 +1,54 @@
 
+#[deriving(Clone, Eq, PartialEq, Show)]
 pub enum Type<Id> {
     Type(Id),
     FunctionType(Vec<Type<Id>>, Box<Type<Id>>)
 }
 
+#[deriving(Clone, PartialEq, Show)]
 pub enum Literal {
     Integer(int),
     Float(f64),
     String(String)
 }
 
+#[deriving(Clone, PartialEq, Show)]
 pub enum Pattern<Id> {
     Constructor(Id, Vec<Id>),
     IdentifierPattern(Id)
 }
 
+#[deriving(Clone, PartialEq, Show)]
 pub struct Alternative<Id> {
     pub pattern: Pattern<Id>,
     pub expression: Expr<Id>
 }
 
+#[deriving(Clone, PartialEq, Show)]
 pub enum Expr<Id> {
     Identifier(Id),
     Literal(Literal),
     Call(Id, Vec<Expr<Id>>),
     IfElse(Box<Expr<Id>>, Box<Expr<Id>>, Box<Expr<Id>>),
     Match(Box<Expr<Id>>, Vec<Alternative<Id>>),
-    Block(Vec<Expr<Id>>)
+    Block(Vec<Expr<Id>>),
+    BinOp(Box<Expr<Id>>, Id, Box<Expr<Id>>)
 }
 
+#[deriving(Clone, PartialEq, Show)]
 pub struct Field<Id> {
     pub name: Id,
     pub typ: Type<Id>
 }
 
+#[deriving(Clone, PartialEq, Show)]
 pub struct Function<Id> {
     pub name: Id,
     pub arguments: Vec<Field<Id>>,
     pub expression: Expr<Id>
 }
 
+#[deriving(Clone, PartialEq, Show)]
 pub struct Struct<Id> {
     pub fields: Vec<Field<Id>>
 }
--- parser.rs
+++ parser.rs
@@ -10,6 +10,22 @@ macro_rules! expect(
         }
     )
 )
+fn precedence(s : &str) -> int {
+    match s {
+        "+" => 1,
+        "-" => 1,
+        "*" => 3,
+        "/" => 3,
+        "%" => 3,
+        "==" => 1,
+        "/=" => 1,
+        "<" => 1,
+        ">" => 1,
+        "<=" => 1,
+        ">=" => 1,
+        _ => 9
+    }
+}
 
 type PString = InternedStr;
 type ParseResult<T> = Result<T, String>;
@@ -24,6 +40,11 @@ impl <'a> Parser<'a> {
     }
 
     fn expression(&mut self) -> ParseResult<Expr<PString>> {
+        let e = try!(self.sub_expression());
+        self.binary_expression(e, 0)
+    }
+
+    fn sub_expression(&mut self) -> ParseResult<Expr<PString>> {
         match *self.lexer.next() {
             TIdentifier(id) => {
                 Ok(Identifier(id))
@@ -33,7 +54,76 @@ impl <'a> Parser<'a> {
                 expect!(self, TCloseParen);
                 Ok(e)
             }
+            TInteger(i) => {
+                Ok(Literal(Integer(i)))
+            }
             _ => Err(String::from_str("Not an expression"))
         }
+    }
+
+    fn binary_expression(&mut self, inL: Expr<PString>, minPrecedence : int) -> ParseResult<Expr<PString>> {
+        let mut lhs = inL;
+        self.lexer.next();
+        loop {
+            let lhs_op;
+            let lhs_prec;
+            match *self.lexer.current() {
+                TOperator(op) => {
+                    lhs_prec = precedence(op.as_slice());
+                    lhs_op = op;
+                    if lhs_prec < minPrecedence {
+                        break
+                    }
+                }
+                _ => break
+            };
+            debug!("Op {}", lhs_op);
+
+            let mut rhs = try!(self.sub_expression());
+            self.lexer.next();
+            loop {
+                let lookahead;
+                match *self.lexer.current() {
+                    TOperator(op) => {
+                        lookahead = precedence(op.as_slice());
+                        if lookahead < lhs_prec {
+                            break
+                        }
+                        debug!("Inner op {}", op);
+                    }
+                    _ => break
+                }
+                self.lexer.backtrack();
+                rhs = try!(self.binary_expression(rhs, lookahead));
+                self.lexer.next();
+            }
+            lhs = BinOp(box lhs, lhs_op.clone(), box rhs)
+        }
+        self.lexer.backtrack();
+        Ok(lhs)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ast::*;
+    use std::io::BufReader;
+    use interner::*;
+
+    fn binop(l: Expr<InternedStr>, s: &str, r: Expr<InternedStr>) -> Expr<InternedStr> {
+        BinOp(box l, intern(s), box r)
+    }
+    fn int(i: int) -> Expr<InternedStr> {
+        Literal(Integer(i))
+    }
+
+    #[test]
+    fn operators() {
+        let mut buffer = BufReader::new("1 / 4 + (2 - 3) * 2".as_bytes());
+        let mut parser = Parser::new(&mut buffer);
+        let expr = parser.expression()
+            .unwrap_or_else(|err| fail!(err));
+        assert_eq!(expr, binop(binop(int(1), "/", int(4)), "+", binop(binop(int(2), "-", int(3)), "*", int(2))));
     }
 }
