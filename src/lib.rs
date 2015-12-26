d663ee5b127ceece8012bc614a6b0d5060d11fd4
Markus <marwes91@gmail.com>
2015-12-26 15:29:55 +0100

    Reexport the sub-crates in the embed_lang crate

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,14 @@
 extern crate log;
 extern crate env_logger;
 
-extern crate base;
-extern crate vm;
-extern crate parser;
-extern crate check;
+mod crates {
+    extern crate base;
+    extern crate parser;
+    extern crate check;
+    extern crate vm;
+}
+
+pub use crates::base;
+pub use crates::parser;
+pub use crates::check;
+pub use crates::vm;
