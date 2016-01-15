6e52fd3f62fb1fd491c790f8052665f9a96388f8
Markus Westerlind <marwes91@gmail.com>
2016-01-16 00:16:10 +0100

    fix(build): Add the test feature to all builds to avoid rebuilds
    Having check require the extra test feature would cause check to always be built twice even if no file had changed

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
-cargo test -p base &&
-    cargo test -p parser &&
+cargo test -p base --features test &&
+    cargo test -p parser --features test &&
     cargo test -p check --features test &&
-    cargo test -p vm &&
-    cargo test &&
+    cargo test -p vm --features test &&
+    cargo test --features test &&
     (cd vm && cargo test --no-default-features)
