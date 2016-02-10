6afc7833ef9d576ebe11174c6844b1d891887db4
Markus Westerlind <marwes91@gmail.com>
2016-02-10 13:29:07 +0100

    feat(stream): Simplify the Stream type

--- std/stream.hs
+++ std/stream.hs
@@ -2,30 +2,30 @@ let prelude = import "std/prelude.hs"
 and { Option, Num } = prelude
 and { (+) } = prelude.num_Int
 in
-type Stream a =
-    | LazyStream (Lazy (Stream a))
+type Stream_ a =
     | Value a (Stream a)
     | Empty
+and Stream a = Lazy (Stream_ a)
 in
 let from f: (Int -> Option a) -> Stream a =
         let from_ i =
-                LazyStream (lazy (\_ -> case f i of
+                lazy (\_ -> case f i of
                     | Some x -> Value x (from_ (i + 1))
                     | None -> Empty
-                ))
+                )
         in from_ 0
 in
-let next stream: Stream a -> { value: Option a, rest: Stream a } =
-    case stream of
-        | LazyStream l -> next (force l)
-        | Value x xs -> { value = Some x, rest = xs }
-        | Empty -> { value = None, rest = Empty }
+let next stream: Stream a -> Option a =
+    case force stream of
+        | Value x _ -> Some x
+        | Empty -> None
+and is_empty stream: Stream a -> Bool =
+    case force stream of
+        | Value _ _ -> False
+        | Empty -> True
+and fold f b stream: (a -> b -> b) -> b -> Stream a -> b =
+    case force stream of
+        | Value x xs -> fold f (f x b) xs
+        | Empty -> b
 in
-let fold f b stream: (a -> b -> b) -> b -> Stream a -> b =
-    let { value, rest } = next stream
-    in
-    case value of
-        | Some a -> fold f (f a b) rest
-        | None -> b
-in
-{ from, next, fold }
+{ from, next, is_empty, fold }
