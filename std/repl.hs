d5c3ba61869dc4a81db604fbe2077e16d7ce381d
Markus Westerlind <marwes91@gmail.com>
2016-02-23 23:04:16 +0100

    refactor: Rewrite repl.hs to rely on implicit `in`

--- std/repl.hs
+++ std/repl.hs
@@ -1,29 +1,29 @@
 let prelude = import "std/prelude.hs"
-and map = import "std/map.hs"
-and string = import "std/string.hs"
-and { Map } = map
-in
+let map = import "std/map.hs"
+let string = import "std/string.hs"
+let { Map } = map
 let { (>>=), return, (>>), join, map = fmap, lift2, forM_ } = prelude.make_Monad prelude.monad_IO
-and { Eq, Option, Result, Monoid } = prelude
-and { (==) } = string.eq
-and (++) = string.monoid.(<>)
-and { singleton, find, insert, monoid, to_list }
+let { Eq, Option, Result, Monoid } = prelude
+let { (==) } = string.eq
+let (++) = string.monoid.(<>)
+let { singleton, find, insert, monoid, to_list }
     = map.make string.ord
-and { (<>), empty } = monoid
-in
+let { (<>), empty } = monoid
+
+
 let load_file filename: String -> IO String =
     let last_slash = (\_ -> case string.rfind filename "/" of
         | None -> 0
-        | Some i -> i #Int+ 1) ()
-    and modulename = string.slice filename last_slash (string.length filename #Int- 3)
-    and read_result = io.catch (io.read_file filename >>= \x -> return (Ok x)) (\err -> return (Err err))
-    in read_result >>= \result ->
+        | Some i -> i + 1) ()
+    let modulename = string.slice filename last_slash (string.length filename - 3)
+    let read_result = io.catch (io.read_file filename >>= \x -> return (Ok x)) (\err -> return (Err err))
+    read_result >>= \result ->
         case result of
             | Ok expr -> io.load_script modulename expr
             | Err msg -> return msg
-in
+
 type Cmd = { info: String, action: String -> IO Bool }
-in
+
 let commands: Map String Cmd
     =  singleton "q" { info = "Quit the REPL", action = \_ -> return False }
         <> singleton "t" {
@@ -38,49 +38,49 @@ let commands: Map String Cmd
             info = "Loads the file at 'folder/module.ext' and stores it at 'module'",
             action = \arg -> load_file arg >>= io.print >> return True
         }
-in
+
 let help =
     let info = "Print this help"
-    in {
-    info,
-    action = \_ ->
-        io.print "Available commands\n" >>
-            io.print ("    :h " ++ info) >>
-            forM_ (to_list commands) (\cmd ->
-                //FIXME This type declaration should not be needed
-                let cmd: { key: String, value: Cmd } = cmd
-                io.print ("    :" ++ cmd.key ++ " " ++ cmd.value.info)
-            ) >>
-            return True
-}
-in
+    {
+        info,
+        action = \_ ->
+            io.print "Available commands\n" >>
+                io.print ("    :h " ++ info) >>
+                forM_ (to_list commands) (\cmd ->
+                    //FIXME This type declaration should not be needed
+                    let cmd: { key: String, value: Cmd } = cmd
+                    io.print ("    :" ++ cmd.key ++ " " ++ cmd.value.info)
+                ) >>
+                return True
+    }
+
 let commands = insert "h" help commands
-in
-let do_command line: String -> IO Bool
-    = 
+let do_command line: String -> IO Bool = 
     let cmd = string.slice line 1 2
-    and arg = string.trim (string.slice line 3 (string.length line))
-    in case find cmd commands of
+    let arg = string.trim (string.slice line 3 (string.length line))
+    case find cmd commands of
         | Some command -> command.action arg
         | None -> io.print ("Unknown command '"  ++ cmd ++ "'") >> return True
-in
-let store line: String -> IO Bool
-    =
+
+let store line: String -> IO Bool =
     let line = string.trim line
-    in case string.find line " " of
+    case string.find line " " of
         | Some bind_end -> 
             let binding = string.slice line 0 bind_end
-            and expr = string.slice line bind_end (string.length line)
-            in io.load_script binding expr >> return True
+            let expr = string.slice line bind_end (string.length line)
+            io.load_script binding expr >> return True
         | None -> io.print "Expected binding in definition" >> return True
-in
+
 let loop x: () -> IO () = io.read_line >>= \line ->
-    (if string.starts_with line ":"
-    then do_command line
-    else if string.starts_with line "def "
-    then store (string.slice line 4 (string.length line))
-    else io.run_expr line >>= io.print >> return True) >>= \continue -> 
-        if continue
-        then loop ()
-        else return ()
-in loop
+    (if string.starts_with line ":" then
+        do_command line
+    else if string.starts_with line "def " then
+        store (string.slice line 4 (string.length line))
+    else
+        io.run_expr line >>= io.print >> return True) >>= \continue -> 
+            if continue then
+                loop ()
+            else
+                return ()
+
+loop
