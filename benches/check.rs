fe1606e6181d9911bad9457826be2af1d5caeb77
Markus Westerlind <marwes91@gmail.com>
2016-04-03 15:40:52 +0200

    fix: Fix compilation errors for benches/vm.rs
    And rename it to check.rs which is more accurate

--- benches/check.rs
+++ benches/check.rs
@@ -10,23 +10,24 @@ extern crate embed_lang;
 use std::fs::File;
 use std::io::Read;
 
-use base::symbol::SymbolModule;
+use base::symbol::{Symbols, SymbolModule};
 use check::typecheck::Typecheck;
 use embed_lang::new_vm;
 
 #[bench]
 fn prelude(b: &mut ::test::Bencher) {
     let vm = new_vm();
-    let env = vm.env();
-    let mut symbols = vm.get_mut_symbols();
-    let expr = {
+    let env = vm.get_env();
+    let mut symbols = Symbols::new();
+    let mut expr = {
         let mut symbols = SymbolModule::new("".into(), &mut symbols);
         let mut text = String::new();
         File::open("std/prelude.hs").unwrap().read_to_string(&mut text).unwrap();
         ::parser::parse_tc(&mut symbols, &text).unwrap_or_else(|err| panic!("{:?}", err))
     };
+    vm.get_macros().run(&vm, &mut expr).unwrap();
     b.iter(|| {
-        let mut tc = Typecheck::new("".into(), &mut symbols, &env);
+        let mut tc = Typecheck::new("".into(), &mut symbols, &*env);
         let result = tc.typecheck_expr(&mut expr.clone());
         if let Err(ref err) = result {
             println!("{}", err);
