7b8471587c0dddc34de5e8cacc63092d0ddb5d13
Markus Westerlind <marwes91@gmail.com>
2016-04-16 17:09:35 +0200

    fix: Remove the testing of vm with --no-default-features
    vm hasnt used any features for some time so there is no need for running it with --no-default-features

--- test.sh
+++ test.sh
@@ -2,5 +2,4 @@ cargo test -p base --features test &&
     cargo test -p parser --features test &&
     cargo test -p check --features test &&
     cargo test -p vm --features test &&
-    cargo test --features test &&
-    (cd vm && cargo test --no-default-features)
+    cargo test --features test
