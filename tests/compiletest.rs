0aaeb213e66dfa32fb2ed82e3e6ecaded9c87879
Markus Westerlind <marwes91@gmail.com>
2016-08-08 23:47:49 +0200

    fix(vm): Allow compiletests to be run from both ./ and ./c-api

--- tests/compiletest.rs
+++ tests/compiletest.rs
@@ -1,18 +1,32 @@
 #![cfg(feature = "nightly")]
 extern crate compiletest_rs as compiletest;
 
+use std::env;
 use std::path::PathBuf;
 
 fn run_mode(mode: &'static str) {
+    // Retrieve the path where library dependencies are output
+    let mut out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    loop {
+        match out_dir.file_name() {
+            Some(name) => {
+                match name.to_str() {
+                    Some(name) if name == "deps" || name == "debug" => break,
+                    _ => (),
+                }
+            }
+            None => break,
+        }
+        out_dir.pop();
+    }
+
     let mut config = compiletest::default_config();
     let cfg_mode = mode.parse().ok().expect("Invalid mode");
 
     config.mode = cfg_mode;
     config.src_base = PathBuf::from(format!("tests/{}", mode));
-    // Pass flags for both c-api and without so both `cargo test ...` and
-    // `(cd c-api && cargo test ... )` works
-    config.target_rustcflags = Some("-L c-api/target/debug -L c-api/target/debug/deps \
-                                     -L target/debug -L target/debug/deps".to_string());
+    let dir = out_dir.to_str().unwrap();
+    config.target_rustcflags = Some(format!("-L {} -L {}/deps", dir, dir));
 
     compiletest::run_tests(&config);
 }
