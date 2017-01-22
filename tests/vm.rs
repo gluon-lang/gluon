8dfc904e5a7813a8624c1a35cef6533303ca3af0
Markus Westerlind <marwes91@gmail.com>
2017-01-22 18:05:08 +0100

    chore: Fix rustc warnings

--- tests/vm.rs
+++ tests/vm.rs
@@ -659,7 +659,7 @@ fn get_binding_with_alias_type() {
 fn get_binding_with_generic_params() {
     let _ = ::env_logger::init();
 
-    let mut vm = make_vm();
+    let vm = make_vm();
     run_expr::<OpaqueValue<&Thread, Hole>>(&vm, r#" import "std/prelude.glu" "#);
     let mut id: FunctionRef<fn(String) -> String> = vm.get_global("std.prelude.id")
         .unwrap_or_else(|err| panic!("{}", err));
