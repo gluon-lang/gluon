f128eae6f760a58dd97611b8dd412a23ac5dad49
Markus Westerlind <marwes91@gmail.com>
2017-05-25 09:29:01 +0200

    chore: Version 0.4.2

--- vm/src/lib.rs
+++ vm/src/lib.rs
@@ -1,5 +1,5 @@
 //! Crate which contain the virtual machine which executes gluon programs
-#![doc(html_root_url="https://docs.rs/gluon_vm/0.4.1")] // # GLUON
+#![doc(html_root_url="https://docs.rs/gluon_vm/0.4.2")] // # GLUON
 
 #[macro_use]
 extern crate log;
