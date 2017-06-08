de3660452a47966ae8547534b358a868544f395c
Markus Westerlind <marwes91@gmail.com>
2017-06-08 23:50:19 +0200

    Rename string.length -> string.len

--- vm/src/primitives.rs
+++ vm/src/primitives.rs
@@ -14,194 +14,197 @@ use stack::StackFrame;
 use thread::ThreadInternal;
 use types::VmInt;
 
-fn array_length(array: Array<generic::A>) -> VmInt {
-    array.len() as VmInt
-}
-
-fn array_index<'vm>(
-    array: Array<'vm, Generic<generic::A>>,
-    index: VmInt,
-) -> RuntimeResult<Generic<generic::A>, String> {
-    match array.get(index) {
-        Some(value) => RuntimeResult::Return(value),
-        None => RuntimeResult::Panic(format!("Index {} is out of range", index)),
-    }
-}
-
-fn array_append<'vm>(
-    lhs: Array<'vm, Generic<generic::A>>,
-    rhs: Array<'vm, Generic<generic::A>>,
-) -> RuntimeResult<Array<'vm, Generic<generic::A>>, Error> {
-    struct Append<'b> {
-        lhs: &'b ValueArray,
-        rhs: &'b ValueArray,
+mod array {
+    fn length(array: Array<generic::A>) -> VmInt {
+        array.len() as VmInt
     }
 
-    impl<'b> Append<'b> {
-        fn repr(&self) -> Repr {
-            // Empty arrays don't have the correct representation set so choose the representation
-            // of `rhs` if it is empty. (And if both are empty the representation does not matter).
-            if self.lhs.len() == 0 {
-                self.rhs.repr()
-            } else {
-                self.lhs.repr()
+    fn index<'vm>(
+        array: Array<'vm, Generic<generic::A>>,
+        index: VmInt,
+    ) -> RuntimeResult<Generic<generic::A>, String> {
+        match array.get(index) {
+            Some(value) => RuntimeResult::Return(value),
+            None => RuntimeResult::Panic(format!("Index {} is out of range", index)),
+        }
+    }
+
+    fn array_append<'vm>(
+        lhs: Array<'vm, Generic<generic::A>>,
+        rhs: Array<'vm, Generic<generic::A>>,
+    ) -> RuntimeResult<Array<'vm, Generic<generic::A>>, Error> {
+        struct Append<'b> {
+            lhs: &'b ValueArray,
+            rhs: &'b ValueArray,
+        }
+        impl<'b> Append<'b> {
+            fn repr(&self) -> Repr {
+                // Empty arrays don't have the correct representation set so choose the representation
+                // of `rhs` if it is empty. (And if both are empty the representation does not matter).
+                if self.lhs.len() == 0 {
+                    self.rhs.repr()
+                } else {
+                    self.lhs.repr()
+                }
             }
         }
-    }
 
-    impl<'b> Traverseable for Append<'b> {
-        fn traverse(&self, gc: &mut Gc) {
-            self.lhs.traverse(gc);
-            self.rhs.traverse(gc);
-        }
-    }
-
-    unsafe impl<'b> DataDef for Append<'b> {
-        type Value = ValueArray;
-        fn size(&self) -> usize {
-            let len = self.lhs.len() + self.rhs.len();
-            ValueArray::size_of(self.repr(), len)
-        }
-        fn initialize<'w>(self, mut result: WriteOnly<'w, ValueArray>) -> &'w mut ValueArray {
-            unsafe {
-                let result = &mut *result.as_mut_ptr();
-                result.set_repr(self.repr());
-                result.initialize(self.lhs.iter().chain(self.rhs.iter()));
-                result
+        impl<'b> Traverseable for Append<'b> {
+            fn traverse(&self, gc: &mut Gc) {
+                self.lhs.traverse(gc);
+                self.rhs.traverse(gc);
             }
         }
-    }
-    let vm = lhs.vm();
-    let value = {
-        let mut context = vm.context();
-        let result = context.alloc(Append {
-            lhs: lhs.get_value_array(),
-            rhs: rhs.get_value_array(),
-        });
-        match result {
-            Ok(x) => x,
-            Err(err) => return RuntimeResult::Panic(err),
+
+        unsafe impl<'b> DataDef for Append<'b> {
+            type Value = ValueArray;
+            fn size(&self) -> usize {
+                let len = self.lhs.len() + self.rhs.len();
+                ValueArray::size_of(self.repr(), len)
+            }
+            fn initialize<'w>(self, mut result: WriteOnly<'w, ValueArray>) -> &'w mut ValueArray {
+                unsafe {
+                    let result = &mut *result.as_mut_ptr();
+                    result.set_repr(self.repr());
+                    result.initialize(self.lhs.iter().chain(self.rhs.iter()));
+                    result
+                }
+            }
         }
-    };
-    unsafe {
-        RuntimeResult::Return(
-            Getable::from_value(lhs.vm(), Variants::new(&Value::Array(value))).expect("Array"),
-        )
+        let vm = lhs.vm();
+        let value = {
+            let mut context = vm.context();
+            let result = context.alloc(Append {
+                lhs: lhs.get_value_array(),
+                rhs: rhs.get_value_array(),
+            });
+            match result {
+                Ok(x) => x,
+                Err(err) => return RuntimeResult::Panic(err),
+            }
+        };
+        unsafe {
+            RuntimeResult::Return(
+                Getable::from_value(lhs.vm(), Variants::new(&Value::Array(value))).expect("Array"),
+            )
+        }
     }
 }
 
-fn string_append(lhs: WithVM<&str>, rhs: &str) -> RuntimeResult<String, Error> {
-    struct StrAppend<'b> {
-        lhs: &'b str,
-        rhs: &'b str,
-    }
-
-    impl<'b> Traverseable for StrAppend<'b> {}
-
-    unsafe impl<'b> DataDef for StrAppend<'b> {
-        type Value = ValueArray;
-        fn size(&self) -> usize {
-            use std::mem::size_of;
-            size_of::<ValueArray>() + (self.lhs.len() + self.rhs.len()) * size_of::<u8>()
+mod string {
+    fn append(lhs: WithVM<&str>, rhs: &str) -> RuntimeResult<String, Error> {
+        struct StrAppend<'b> {
+            lhs: &'b str,
+            rhs: &'b str,
         }
-        fn initialize<'w>(self, mut result: WriteOnly<'w, ValueArray>) -> &'w mut ValueArray {
-            use value::Repr;
-            unsafe {
-                let result = &mut *result.as_mut_ptr();
-                result.set_repr(Repr::Byte);
-                result.unsafe_array_mut::<u8>().initialize(
-                    self.lhs
-                        .as_bytes()
-                        .iter()
-                        .chain(self.rhs.as_bytes())
-                        .cloned(),
-                );
-                result
+
+        impl<'b> Traverseable for StrAppend<'b> {}
+
+        unsafe impl<'b> DataDef for StrAppend<'b> {
+            type Value = ValueArray;
+            fn size(&self) -> usize {
+                use std::mem::size_of;
+                size_of::<ValueArray>() + (self.lhs.len() + self.rhs.len()) * size_of::<u8>()
+            }
+            fn initialize<'w>(self, mut result: WriteOnly<'w, ValueArray>) -> &'w mut ValueArray {
+                use value::Repr;
+                unsafe {
+                    let result = &mut *result.as_mut_ptr();
+                    result.set_repr(Repr::Byte);
+                    result.unsafe_array_mut::<u8>().initialize(
+                        self.lhs
+                            .as_bytes()
+                            .iter()
+                            .chain(self.rhs.as_bytes())
+                            .cloned(),
+                    );
+                    result
+                }
             }
         }
-    }
 
-    let vm = lhs.vm;
-    let lhs = lhs.value;
-    unsafe {
-        let mut context = vm.context();
-        let result = context.alloc(StrAppend { lhs: lhs, rhs: rhs });
-        let value = match result {
-            Ok(x) => GcStr::from_utf8_unchecked(x),
-            Err(err) => return RuntimeResult::Panic(err),
+        let vm = lhs.vm;
+        let lhs = lhs.value;
+        let value = unsafe {
+            let mut context = vm.context();
+            let result = context.alloc(StrAppend { lhs: lhs, rhs: rhs });
+            match result {
+                Ok(x) => GcStr::from_utf8_unchecked(x),
+                Err(err) => return RuntimeResult::Panic(err),
+            }
         };
         RuntimeResult::Return(
-            Getable::from_value(vm, Variants::new(&Value::String(value))).expect("Array"),
+            Getable::from_value(vm, Variants(&Value::String(value))).expect("Array"),
         )
     }
-}
 
-fn string_slice(s: &str, start: usize, end: usize) -> RuntimeResult<&str, String> {
-    if s.is_char_boundary(start) && s.is_char_boundary(end) {
-        RuntimeResult::Return(&s[start..end])
-    } else {
-        // Limit the amount of characters to print in the error message
-        let mut iter = s.chars();
-        for _ in iter.by_ref().take(256) {}
-        RuntimeResult::Panic(format!(
-            "index {} and/or {} in `{}` does not lie on a character \
-             boundary",
-            start,
-            end,
-            &s[..(s.len() - iter.as_str().len())]
-        ))
+    fn slice(s: &str, start: usize, end: usize) -> RuntimeResult<&str, String> {
+        if s.is_char_boundary(start) && s.is_char_boundary(end) {
+            RuntimeResult::Return(&s[start..end])
+        } else {
+            // Limit the amount of characters to print in the error message
+            let mut iter = s.chars();
+            for _ in iter.by_ref().take(256) {}
+            RuntimeResult::Panic(format!(
+                "index {} and/or {} in `{}` does not lie on a character \
+                 boundary",
+                start,
+                end,
+                &s[..(s.len() - iter.as_str().len())]
+            ))
+        }
     }
-}
 
-extern "C" fn from_utf8(thread: &Thread) -> Status {
-    let mut context = thread.context();
-    let value = StackFrame::current(&mut context.stack)[0];
-    match value {
-        Value::Array(array) => {
-            match GcStr::from_utf8(array) {
-                Ok(string) => {
-                    let value = Value::String(string);
-                    let result = context.alloc_with(
-                        thread,
-                        Def {
-                            tag: 1,
-                            elems: &[value],
-                        },
-                    );
-                    match result {
-                        Ok(data) => {
-                            context.stack.push(Value::Data(data));
-                            Status::Ok
-                        }
-                        Err(err) => {
-                            let result: RuntimeResult<(), _> = RuntimeResult::Panic(err);
-                            result.status_push(thread, &mut context)
+    extern "C" fn from_utf8(thread: &Thread) -> Status {
+        let mut context = thread.context();
+        let value = StackFrame::current(&mut context.stack)[0];
+        match value {
+            Value::Array(array) => {
+                match GcStr::from_utf8(array) {
+                    Ok(string) => {
+                        let value = Value::String(string);
+                        let result = context.alloc_with(
+                            thread,
+                            Def {
+                                tag: 1,
+                                elems: &[value],
+                            },
+                        );
+                        match result {
+                            Ok(data) => {
+                                context.stack.push(Value::Data(data));
+                                Status::Ok
+                            }
+                            Err(err) => {
+                                let result: RuntimeResult<(), _> = RuntimeResult::Panic(err);
+                                result.status_push(thread, &mut context)
+                            }
                         }
                     }
-                }
-                Err(()) =>{
+                    Err(()) =>{
                     let err: StdResult<&str, ()> = Err(());
                     err.status_push(thread, &mut context)
                 }
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    fn char_at(s: &str, index: usize) -> RuntimeResult<char, String> {
+        if s.is_char_boundary(index) {
+            if let Some(c) = s[index..].chars().next() {
+                return RuntimeResult::Return(c);
             }
         }
-        _ => unreachable!(),
+        let mut iter = s.chars();
+        for _ in iter.by_ref().take(256) {}
+        RuntimeResult::Panic(format!(
+            "index {} in `{}` does not lie on a character boundary",
+            index,
+            &s[..(s.len() - iter.as_str().len())]
+        ))
     }
-}
-
-fn char_at(s: &str, index: usize) -> RuntimeResult<char, String> {
-    if s.is_char_boundary(index) {
-        if let Some(c) = s[index..].chars().next() {
-            return RuntimeResult::Return(c);
-        }
-    }
-    let mut iter = s.chars();
-    for _ in iter.by_ref().take(256) {}
-    RuntimeResult::Panic(format!(
-        "index {} in `{}` does not lie on a character boundary",
-        index,
-        &s[..(s.len() - iter.as_str().len())]
-    ))
 }
 
 fn show_int(i: VmInt) -> String {
@@ -288,11 +291,13 @@ pub fn load(vm: &Thread) -> Result<()> {
         tanh => primitive!(1 float::tanh),
         acosh => primitive!(1 float::acosh),
         atanh => primitive!(1 float::atanh)
-    ))?;
+    ),
+    )?;
 
     use types::VmInt as int;
-    vm.define_global("int",
-                       record!(
+    vm.define_global(
+        "int",
+        record!(
         min_value => int::min_value(),
         max_value => int::max_value(),
         count_ones => primitive!(1 int::count_ones),
@@ -308,22 +313,26 @@ pub fn load(vm: &Thread) -> Result<()> {
         signum => primitive!(1 int::signum),
         is_positive => primitive!(1 int::is_positive),
         is_negative => primitive!(1 int::is_negative)
-    ))?;
+    ),
+    )?;
 
     {
-        use self::prim as array;
-        vm.define_global("array",
-                           record!(
-            length => primitive!(1 array::array_length),
-            index => primitive!(2 array::array_index),
-            append => primitive!(2 array::array_append)
-        ))?;
+        use self::array;
+        vm.define_global(
+            "array",
+            record!(
+            length => primitive!(1 array::length),
+            index => primitive!(2 array::index),
+            append => primitive!(2 array::append)
+        ),
+        )?;
     }
 
     type string_prim = str;
-    vm.define_global("string_prim",
-                       record!(
-        length => primitive!(1 string_prim::len),
+    vm.define_global(
+        "string_prim",
+        record!(
+        len => primitive!(1 string_prim::len),
         is_empty => primitive!(1 string_prim::is_empty),
         split_at => primitive!(2 string_prim::split_at),
         find => primitive!(2 string_prim::find::<&str>),
@@ -334,7 +343,7 @@ pub fn load(vm: &Thread) -> Result<()> {
         trim_left => primitive!(1 string_prim::trim_left),
         trim_right => primitive!(1 string_prim::trim_right),
         compare => primitive!(2 string_prim::cmp),
-        append => named_primitive!(2, "string_prim.append", prim::string_append),
+        append => named_primitive!(2, "string_prim.append", string_prim::string_append),
         eq => named_primitive!(2, "string_prim.eq", <str as PartialEq>::eq),
         slice => named_primitive!(3, "string_prim.slice", prim::string_slice),
         from_utf8 =>
@@ -342,7 +351,8 @@ pub fn load(vm: &Thread) -> Result<()> {
                                                               prim::from_utf8),
         char_at => named_primitive!(2, "string_prim.char_at", prim::char_at),
         as_bytes => primitive!(1 string_prim::as_bytes)
-    ))?;
+    ),
+    )?;
     vm.define_global(
         "char",
         record!(
@@ -357,14 +367,16 @@ pub fn load(vm: &Thread) -> Result<()> {
         is_alphanumeric => primitive!(1 char::is_alphanumeric),
         is_control => primitive!(1 char::is_control),
         is_numeric => primitive!(1 char::is_numeric)
-    ))?;
+    ),
+    )?;
     vm.define_global(
         "prim",
         record!(
         show_int => primitive!(1 prim::show_int),
         show_float => primitive!(1 prim::show_float),
         show_char => primitive!(1 prim::show_char)
-    ))?;
+    ),
+    )?;
 
     vm.define_global(
         "#error",
