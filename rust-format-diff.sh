79e40eaead3956adea68ef43f7604fe1ab486e25
Justin <justin.j.greene@gmail.com>
2017-07-09 15:27:30 -0500

    add rust format scripts for easily formatting all cargo packages and checking to see if running rustfmt would result in changes

---
+++ rust-format-diff.sh
@@ -0,0 +1,8 @@
+for d in $(find . -name "Cargo.toml" -printf '%h\n')
+do
+    (cd $d; cargo fmt -- --write-mode=diff)
+    status=$?
+    if [ $status -ne 0 ] && [ $status -ne 101  ]; then # 101 is due to a bug in rustfmt that the ./vm package is triggering
+        exit $status;
+    fi
+done
