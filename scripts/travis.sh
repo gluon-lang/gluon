644a2ce088ab2046ea2e491da8da0d8f78551a01
Markus Westerlind <marwes91@gmail.com>
2017-07-10 23:16:24 +0200

    Ensure travis-cargo's environment variable is set correctly

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-RUST_BACKTRACE=1 cargo test --features test --all &&
+
+(
+  export TRAVIS_CARGO_NIGHTLY_FEATURE="nightly";
+  export RUST_BACKTRACE=1;
+  cargo test --features test --all &&
   travis-cargo --only nightly test -- --features "test nightly" -p gluon compile_test &&
   travis-cargo --only nightly bench &&
   travis-cargo --only stable build -- --all --no-default-features
+)
