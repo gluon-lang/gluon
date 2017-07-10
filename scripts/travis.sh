aa81bd2bb0dec58066dae0751a5f5f648ff7fad0
Markus Westerlind <marwes91@gmail.com>
2017-07-10 23:36:52 +0200

    chore: Only check the benchmarks to speedup travis builds

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -5,6 +5,6 @@
   export RUST_BACKTRACE=1;
   cargo test --features test --all &&
   travis-cargo --only nightly test -- --features "test nightly" -p gluon compile_test &&
-  travis-cargo --only nightly bench &&
+  cargo check --benches --features test &&
   travis-cargo --only stable build -- --all --no-default-features
 )
