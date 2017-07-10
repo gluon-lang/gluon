fe0ec77ad259582c0694d3d371967025f8bc7486
Markus Westerlind <marwes91@gmail.com>
2017-07-10 22:05:12 +0200

    Enable backtrace for travis builds

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-
-cargo test --features test --all &&
+RUST_BACKTRACE=1 cargo test --features test --all &&
   travis-cargo --only nightly test -- --features "test nightly" -p gluon compile_test &&
   travis-cargo --only nightly bench &&
   travis-cargo --only stable build -- --all --no-default-features
