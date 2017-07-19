cb53d131e2606f88bd25a9a0e67841b5df437d0b
Markus Westerlind <marwes91@gmail.com>
2017-07-19 21:41:48 +0200

    chore: Avid using --all to avoid travis running out of memory

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -3,7 +3,13 @@
 (
   export TRAVIS_CARGO_NIGHTLY_FEATURE="nightly";
   export RUST_BACKTRACE=1;
-  cargo test --features test --all &&
+  cargo test --features test --package gluon_base &&
+  cargo test --features test --package gluon_parser &&
+  cargo test --features test --package gluon_check &&
+  cargo test --features test --package gluon_vm &&
+  cargo test --features test --package gluon &&
+  cargo test --features test --package gluon_repl &&
+  cargo test --features test --package gluon_c-api &&
   travis-cargo --only nightly test -- --features "test nightly" -p gluon compile_test &&
   cargo check --bench check --features test &&
   cargo check --bench function_call --features test &&
