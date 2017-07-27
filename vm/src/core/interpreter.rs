8d32f3bb6a417ee98c3925728e908941c6b08ceb
Markus <marwes91@gmail.com>
2017-07-27 13:47:09 +0200

    Allow globals to be stored an propagated in the core interpreter

--- vm/src/core/interpreter.rs
+++ vm/src/core/interpreter.rs
@@ -22,6 +22,45 @@ pub type CExpr<'a> = &'a core::Expr<'a>;
 
 pub struct FreeVars(FnvSet<Symbol>);
 
+#[derive(Copy, Clone, Debug)]
+enum ReducedExpr<'l, 'g> {
+    Local(CExpr<'l>),
+    Global(CExpr<'g>),
+}
+
+macro_rules! match_reduce {
+    ($expr: expr, $wrapper: ident; $($pat: pat => $alt: expr),*) => {
+        match $expr {
+            ReducedExpr::Local(e) => {
+                let $wrapper = ReducedExpr::Local;
+                match *e {
+                    $($pat => $alt),*
+                }
+            }
+            ReducedExpr::Global(e) => {
+                let $wrapper = ReducedExpr::Global;
+                match *e {
+                    $($pat => $alt),*
+                }
+            }
+        }
+    }
+}
+
+impl<'l, 'g> ReducedExpr<'l, 'g> {
+    fn as_ref(&self) -> &Expr {
+        match *self {
+            ReducedExpr::Local(e) | ReducedExpr::Global(e) => e,
+        }
+    }
+}
+
+impl <'l, 'g> From<CExpr<'l>> for ReducedExpr<'l, 'g> {
+    fn from(expr: CExpr<'l>) -> Self {
+        ReducedExpr::Local(expr)
+    }
+}
+
 impl<'e> Visitor<'e> for FreeVars {
     fn visit_expr(&mut self, expr: CExpr<'e>) -> Option<CExpr<'e>> {
         match *expr {
@@ -59,8 +98,8 @@ impl<'e> Visitor<'e> for Pure {
 }
 
 #[derive(Clone, Debug)]
-pub enum Variable<'a, G> {
-    Stack(Option<&'a Expr<'a>>),
+pub enum Variable<'l, 'g, G> {
+    Stack(Option<ReducedExpr<'l, 'g>>),
     Global(G),
     Constructor(VmTag, VmIndex),
 }
@@ -77,30 +116,30 @@ enum TailCall<'a, T> {
     Value(T),
 }
 
-struct FunctionEnv<'a> {
+struct FunctionEnv<'l, 'g> {
     /// The variables currently in scope in the this function.
-    stack: ScopedMap<Symbol, Option<&'a Expr<'a>>>,
+    stack: ScopedMap<Symbol, Option<ReducedExpr<'l, 'g>>>,
 }
 
-struct FunctionEnvs<'a> {
-    envs: Vec<FunctionEnv<'a>>,
+struct FunctionEnvs<'l, 'g> {
+    envs: Vec<FunctionEnv<'l, 'g>>,
 }
 
-impl<'a> Deref for FunctionEnvs<'a> {
-    type Target = FunctionEnv<'a>;
-    fn deref(&self) -> &FunctionEnv<'a> {
+impl<'l, 'g> Deref for FunctionEnvs<'l, 'g> {
+    type Target = FunctionEnv<'l, 'g>;
+    fn deref(&self) -> &FunctionEnv<'l, 'g> {
         self.envs.last().expect("FunctionEnv")
     }
 }
 
-impl<'a> DerefMut for FunctionEnvs<'a> {
-    fn deref_mut(&mut self) -> &mut FunctionEnv<'a> {
+impl<'l, 'g> DerefMut for FunctionEnvs<'l, 'g> {
+    fn deref_mut(&mut self) -> &mut FunctionEnv<'l, 'g> {
         self.envs.last_mut().expect("FunctionEnv")
     }
 }
 
-impl<'a> FunctionEnvs<'a> {
-    fn new() -> FunctionEnvs<'a> {
+impl<'l, 'g> FunctionEnvs<'l, 'g> {
+    fn new() -> FunctionEnvs<'l, 'g> {
         FunctionEnvs { envs: vec![] }
     }
 
@@ -110,15 +149,15 @@ impl<'a> FunctionEnvs<'a> {
         self.envs.push(FunctionEnv::new());
     }
 
-    fn end_function(&mut self, compiler: &mut Compiler) -> FunctionEnv<'a> {
+    fn end_function(&mut self, compiler: &mut Compiler) -> FunctionEnv<'l, 'g> {
         compiler.stack_types.exit_scope();
         compiler.stack_constructors.exit_scope();
         self.envs.pop().expect("FunctionEnv in scope")
     }
 }
 
-impl<'a> FunctionEnv<'a> {
-    fn new() -> FunctionEnv<'a> {
+impl<'l, 'g> FunctionEnv<'l, 'g> {
+    fn new() -> FunctionEnv<'l, 'g> {
         FunctionEnv {
             stack: ScopedMap::new(),
         }
@@ -128,15 +167,15 @@ impl<'a> FunctionEnv<'a> {
         self.new_stack_var(compiler, s, None)
     }
 
-    fn push_stack_var(&mut self, compiler: &Compiler, s: Symbol, expr: &'a Expr<'a>) {
+    fn push_stack_var(&mut self, compiler: &Compiler, s: Symbol, expr: ReducedExpr<'l, 'g>) {
         self.new_stack_var(compiler, s, Some(expr))
     }
 
-    fn new_stack_var(&mut self, compiler: &Compiler, s: Symbol, mut expr: Option<&'a Expr<'a>>) {
+    fn new_stack_var(&mut self, compiler: &Compiler, s: Symbol, mut expr: Option<ReducedExpr<'l, 'g>>) {
         expr = expr.and_then(|expr| {
             // Only allow pure expression to be folded
             let mut p = Pure(true);
-            p.visit_expr(expr);
+            p.visit_expr(expr.as_ref());
             if p.0 {
                 Some(expr)
             } else {
@@ -158,6 +197,7 @@ impl<'a> FunctionEnv<'a> {
 pub struct Compiler<'a, 'e> {
     allocator: &'e Allocator<'e>,
     symbols: SymbolModule<'a>,
+    globals: &'a Fn(&Symbol) -> Option<CExpr<'a>>,
     stack_constructors: ScopedMap<Symbol, ArcType>,
     stack_types: ScopedMap<Symbol, Alias<Symbol, ArcType>>,
 }
@@ -187,16 +227,17 @@ impl<'a, 'e> TypeEnv for Compiler<'a, 'e> {
 }
 
 impl<'a, 'e> Compiler<'a, 'e> {
-    pub fn new(allocator: &'e Allocator<'e>, mut symbols: SymbolModule<'a>) -> Compiler<'a, 'e> {
+    pub fn new(allocator: &'e Allocator<'e>, mut symbols: SymbolModule<'a>, globals: &'a Fn(&Symbol) -> Option<CExpr<'a>>) -> Compiler<'a, 'e> {
         Compiler {
             allocator,
             symbols,
+            globals,
             stack_constructors: ScopedMap::new(),
             stack_types: ScopedMap::new(),
         }
     }
 
-    fn find(&self, id: &Symbol, current: &mut FunctionEnvs<'e>) -> Option<Variable<'e, VmIndex>> {
+    fn find(&self, id: &Symbol, current: &mut FunctionEnvs<'e, 'a>) -> Option<Variable<'e, 'a, CExpr<'a>>> {
         let variable = self.stack_constructors
             .iter()
             .filter_map(|(_, typ)| match **typ {
@@ -223,10 +264,11 @@ impl<'a, 'e> Compiler<'a, 'e> {
                         .filter_map(|env| env.stack.get(id).map(|&expr| Stack(expr)))
                         .next()
                 })
-            });
-        variable.and_then(|variable: Variable<'e, VmIndex>| match variable {
+            })
+            .or_else(|| (self.globals)(id).map(Variable::Global));
+        variable.and_then(|variable| match variable {
             Stack(i) => Some(Stack(i)),
-            Global(s) => None,
+            Global(s) => Some(Global(s)),
             Constructor(tag, args) => Some(Constructor(tag, args)),
         })
     }
@@ -258,26 +300,26 @@ impl<'a, 'e> Compiler<'a, 'e> {
         debug!("Interpreting: {}", expr);
         let new_expr = self.compile(expr, &mut env)?;
         env.end_function(self);
-        Ok(new_expr.unwrap_or(expr))
+        Ok(new_expr.and_then(|expr| match expr { ReducedExpr::Local(e) => Some(e), ReducedExpr::Global(_) => None, }).unwrap_or(expr))
     }
 
     fn load_identifier(
         &self,
         id: &Symbol,
-        function: &mut FunctionEnvs<'e>,
-    ) -> Result<Option<&'e Expr<'e>>> {
+        function: &mut FunctionEnvs<'e, 'a>,
+    ) -> Result<Option<ReducedExpr<'e, 'a>>> {
         match self.find(id, function) {
             Some(variable) => {
                 match variable {
                     Stack(expr) => {
                         if let Some(e) = expr {
-                            debug!("Loading `{}` as `{}`", id, e);
+                            debug!("Loading `{}` as `{}`", id, e.as_ref());
                         } else {
                             debug!("Unable to fold `{}`", id);
                         }
                         Ok(expr)
                     }
-                    Global(index) => Ok(None),
+                    Global(expr) => Ok(Some(ReducedExpr::Global(expr))),
                     Constructor(..) => Ok(None),
                 }
             }
@@ -289,11 +331,11 @@ impl<'a, 'e> Compiler<'a, 'e> {
     fn walk_expr(
         &mut self,
         expr: CExpr<'e>,
-        functions: &mut FunctionEnvs<'e>,
-    ) -> Result<Option<CExpr<'e>>> {
+        functions: &mut FunctionEnvs<'e, 'a>,
+    ) -> Result<Option<ReducedExpr<'e, 'a>>> {
         struct ReplaceVisitor<'a: 'f, 'e: 'f, 'f> {
             compiler: &'f mut Compiler<'a, 'e>,
-            functions: &'f mut FunctionEnvs<'e>,
+            functions: &'f mut FunctionEnvs<'e, 'a>,
             error: Option<Error>,
         }
         impl<'a, 'e, 'f> Visitor<'e> for ReplaceVisitor<'a, 'e, 'f> {
@@ -303,7 +345,9 @@ impl<'a, 'e> Compiler<'a, 'e> {
                         self.error = Some(err);
                         None
                     }
-                    Ok(expr) => expr,
+                    Ok(Some(ReducedExpr::Local(expr))) => Some(expr),
+                    // FIXME Convert global expressions into local
+                    _ => None,
                 }
             }
             fn allocator(&self) -> &'e Allocator<'e> {
@@ -319,15 +363,15 @@ impl<'a, 'e> Compiler<'a, 'e> {
         let new_expr = walk_expr_alloc(&mut visitor, expr);
         match visitor.error {
             Some(err) => Err(err),
-            None => Ok(new_expr),
+            None => Ok(new_expr.map(ReducedExpr::Local)),
         }
     }
 
     fn compile(
         &mut self,
         mut expr: CExpr<'e>,
-        function: &mut FunctionEnvs<'e>,
-    ) -> Result<Option<CExpr<'e>>> {
+        function: &mut FunctionEnvs<'e, 'a>,
+    ) -> Result<Option<ReducedExpr<'e, 'a>>> {
         // Store a stack of expressions which need to be cleaned up after this "tailcall" loop is
         // done
         function.stack.enter_scope();
@@ -342,10 +386,10 @@ impl<'a, 'e> Compiler<'a, 'e> {
     fn compile_(
         &mut self,
         expr: CExpr<'e>,
-        function: &mut FunctionEnvs<'e>,
-    ) -> Result<TailCall<'e, Option<CExpr<'e>>>> {
+        function: &mut FunctionEnvs<'e, 'a>,
+    ) -> Result<TailCall<'e, Option<ReducedExpr<'e, 'a>>>> {
         let reduced = match *expr {
-            Expr::Const(_, _) => Some(expr),
+            Expr::Const(_, _) => Some(ReducedExpr::Local(expr)),
             Expr::Ident(ref id, _) => self.load_identifier(&id.name, function)?,
             Expr::Let(ref let_binding, ref body) => {
                 self.stack_constructors.enter_scope();
@@ -355,7 +399,7 @@ impl<'a, 'e> Compiler<'a, 'e> {
                         function.push_stack_var(
                             self,
                             let_binding.name.name.clone(),
-                            reduced.unwrap_or(bind_expr),
+                            reduced.unwrap_or(ReducedExpr::Local(bind_expr)),
                         );
                     }
                     core::Named::Recursive(ref closures) => {
@@ -382,8 +426,8 @@ impl<'a, 'e> Compiler<'a, 'e> {
                 self.walk_expr(expr, function)?
             }
             Expr::Match(expr, alts) => {
-                let expr = self.compile(expr, function)?.unwrap_or(expr);
-                let typ = expr.env_type_of(self);
+                let expr = self.compile(expr, function)?.unwrap_or(ReducedExpr::Local(expr));
+                let typ = expr.as_ref().env_type_of(self);
                 for alt in alts {
                     self.stack_constructors.enter_scope();
                     function.stack.enter_scope();
@@ -394,20 +438,20 @@ impl<'a, 'e> Compiler<'a, 'e> {
                             }
                         }
                         Pattern::Record { .. } => {
-                            let typ = &expr.env_type_of(self);
+                            let typ = &expr.as_ref().env_type_of(self);
                             self.compile_let_pattern(&alt.pattern, expr, typ, function)?;
                         }
                         Pattern::Ident(ref id) => {
                             function.push_stack_var(self, id.name.clone(), expr);
                         }
                     }
-                    let new_expr = self.compile(&alt.expr, function)?.unwrap_or(&alt.expr);
+                    let new_expr = self.compile(&alt.expr, function)?.unwrap_or(ReducedExpr::Local(&alt.expr));
                     function.exit_scope(self);
                     self.stack_constructors.exit_scope();
                     match alt.pattern {
                         Pattern::Record(ref fields) if alts.len() == 1 => {
                             let mut free_vars = FreeVars(FnvSet::default());
-                            free_vars.visit_expr(new_expr);
+                            free_vars.visit_expr(new_expr.as_ref());
                             let free_vars_in_expr = fields.iter().any(|field| {
                                 let field_name = field.1.as_ref().unwrap_or(&field.0.name);
                                 free_vars.0.contains(field_name)
@@ -430,9 +474,9 @@ impl<'a, 'e> Compiler<'a, 'e> {
     fn compile_let_pattern(
         &mut self,
         pattern: &Pattern,
-        pattern_expr: &'e Expr<'e>,
+        pattern_expr: ReducedExpr<'e, 'a>,
         pattern_type: &ArcType,
-        function: &mut FunctionEnvs<'e>,
+        function: &mut FunctionEnvs<'e, 'a>,
     ) -> Result<()> {
         match *pattern {
             Pattern::Ident(ref name) => {
@@ -440,7 +484,9 @@ impl<'a, 'e> Compiler<'a, 'e> {
             }
             Pattern::Record(ref fields) => {
                 let typ = resolve::remove_aliases(self, pattern_type.clone());
-                match *pattern_expr {
+                match_reduce!{
+                    pattern_expr, wrap;
+
                     Expr::Data(ref data_id, ref exprs, _, _) => {
                         for pattern_field in fields {
                             let field = data_id
@@ -453,10 +499,10 @@ impl<'a, 'e> Compiler<'a, 'e> {
                                 .as_ref()
                                 .unwrap_or(&pattern_field.0.name)
                                 .clone();
-                            function.push_stack_var(self, field_name, &exprs[field]);
+                            function.push_stack_var(self, field_name, wrap(&exprs[field]));
                         }
-                    }
-                    _ => panic!("Expected record, got {} at {:?}", typ, pattern),
+                    },
+                    _ => panic!("Expected record, got {} at {:?}", typ, pattern)
                 }
             }
             Pattern::Constructor(..) => panic!("constructor pattern in let"),
@@ -469,7 +515,7 @@ impl<'a, 'e> Compiler<'a, 'e> {
         id: &TypedIdent,
         args: &[TypedIdent],
         body: CExpr<'e>,
-        function: &mut FunctionEnvs<'e>,
+        function: &mut FunctionEnvs<'e, 'a>,
     ) -> Result<()> {
         function.start_function(self);
 
@@ -497,6 +543,7 @@ mod tests {
 
     macro_rules! assert_eq_expr {
         ($actual: expr, $expected: expr) => { {
+            let globals = |_| None;
             let mut symbols = Symbols::new();
 
             let allocator = Allocator::new();
@@ -506,7 +553,7 @@ mod tests {
 
             let actual_expr = {
                 let symbols = SymbolModule::new("test".to_string(), &mut symbols);
-                Compiler::new(&allocator, symbols)
+                Compiler::new(&allocator, symbols, &globals)
                     .compile_expr(allocator.arena.alloc(actual_expr))
                     .unwrap()
             };
