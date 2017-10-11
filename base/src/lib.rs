456ab3a3902eeb72cceb5de2560c9cb3edb9a044
Markus Westerlind <marwes91@gmail.com>
2017-10-11 19:56:22 +0200

    chore: Bump to version 0.6.1

--- base/src/lib.rs
+++ base/src/lib.rs
@@ -1,6 +1,6 @@
 //! The base crate contains pervasive types used in the compiler such as type representations, the
 //! AST and some basic containers.
-#![doc(html_root_url = "https://docs.rs/gluon_base/0.6.0")] // # GLUON
+#![doc(html_root_url = "https://docs.rs/gluon_base/0.6.1")] // # GLUON
 
 #[macro_use]
 extern crate collect_mac;
