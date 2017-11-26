fd7ff461f4427e0b95bce8bb0e40de9268b8539b
Markus Westerlind <marwes91@gmail.com>
2017-11-26 16:44:29 +0100

    chore: Write lalrpop files to OUT_DIR instead of the source tree
    Should let gluon be ran under cargobomb https://github.com/rust-lang-nursery/crater/issues/157 but I think that regardless of that it is better to generate files outside of the source tree

--- parser/build.rs
+++ parser/build.rs
@@ -3,6 +3,9 @@ extern crate lalrpop;
 fn main() {
     ::std::env::set_var("LALRPOP_LANE_TABLE", "disabled");
 
-    lalrpop::Configuration::new().process_current_dir().unwrap();
-    println!("cargo:rerun-if-changed=src/grammar.lalrpop");
+    lalrpop::Configuration::new()
+        .use_cargo_dir_conventions()
+        .process_file("src/grammar.lalrpop")
+        .unwrap();
+    println!("cargo:rerun-if-changed=grammar.lalrpop");
 }
