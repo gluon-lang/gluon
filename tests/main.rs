20a63a8962eaf0fc5dc0ab8ecfa0ca0339c90fa9
Markus Westerlind <marwes91@gmail.com>
2017-11-27 21:33:53 +0100

    Revert "refactor: Move gluon script tests to its own crate"
    This reverts commit 3b7073f2fb5e4f9130f49b702200de08743c2b6c.

--- tests/main.rs
+++ tests/main.rs
@@ -28,6 +28,7 @@ impl Error for StringError {
     }
 }
 
+#[test]
 fn main() {
     if let Err(err) = main_() {
         assert!(false, "{}", err);
@@ -69,7 +70,6 @@ fn run_file<'t>(
 
 fn main_() -> Result<(), Box<Error>> {
     let _ = ::env_logger::init();
-
     let args: Vec<_> = ::std::env::args().collect();
     let filter = if args.len() > 1 && args.last().unwrap() != "main" {
         args.last()
@@ -78,13 +78,6 @@ fn main_() -> Result<(), Box<Error>> {
     };
 
     let vm = new_vm();
-    let import = vm.get_macros().get("import");
-    let import = import
-        .as_ref()
-        .and_then(|import| import.downcast_ref::<gluon::import::Import>())
-        .unwrap();
-    import.add_path("..");
-
     Compiler::new()
         .load_file_async(&vm, "std/prelude.glu")
         .sync_or_error()?;
