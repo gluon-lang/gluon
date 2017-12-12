4a744819d68c241f47292cbaf754aa81027fd7b7
Markus Westerlind <marwes91@gmail.com>
2017-12-12 19:52:48 +0100

    chore: Remove the need for travis-cargo

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 (
-  export TRAVIS_CARGO_NIGHTLY_FEATURE="nightly";
   export RUST_BACKTRACE=1;
   cargo test --features test --all &&
   cargo check --benches --features test &&
-  travis-cargo --only nightly test -- --features "test nightly" -p gluon compile_test &&
-  travis-cargo --only stable build -- --all --no-default-features
+  cargo check --all --no-default-features &&
+  ([ "$TRAVIS_RUST_VERSION" != "nightly" ] || cargo test --features "test nightly" -p gluon compile_test)
 )
