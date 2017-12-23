fc91852c330c9eeb33330afc7276753480181b4a
Markus <marwes91@gmail.com>
2017-12-23 17:01:51 +0100

    chore: Use a fixed nightly version to get compiletest working

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -5,5 +5,6 @@
   cargo test --features test --all &&
   cargo check --benches --features test &&
   cargo check --all --no-default-features &&
+  echo "TRAVIS_RUST_VERSION=$TRAVIS_RUST_VERSION" &&
   ([ "$TRAVIS_RUST_VERSION" != "nightly" ] || cargo test --features "test nightly" -p gluon compile_test)
 )
