646b7c66d384209cb57bdddb2aa14688e287ba67
Markus Westerlind <marwes91@gmail.com>
2018-01-24 23:24:19 +0100

    feat: Export expr pretty printing function to format

--- format/src/lib.rs
+++ format/src/lib.rs
@@ -1,19 +1,20 @@
 //! Code formatter.
 #![doc(html_root_url = "https://docs.rs/gluon_formatter/0.7.0")] // # GLUON
 
-extern crate itertools;
-extern crate pretty;
 #[macro_use]
 extern crate gluon_base as base;
 extern crate gluon_parser as parser;
+extern crate itertools;
+extern crate pretty;
+
+use base::ast::SpannedExpr;
+use base::source::Source;
+use base::symbol::{Symbol, Symbols};
+use base::types::TypeCache;
 
 mod pretty_print;
 
-pub fn format_expr(input: &str) -> Result<String, parser::ParseErrors> {
-    use base::source::Source;
-    use base::symbol::Symbols;
-    use base::types::TypeCache;
-
+pub fn pretty_expr(expr: &SpannedExpr<Symbol>, input: &str) -> String {
     let newline = match input.find(|c: char| c == '\n' || c == '\r') {
         Some(i) => {
             if input[i..].starts_with("\r\n") {
@@ -27,11 +28,15 @@ pub fn format_expr(input: &str) -> Result<String, parser::ParseErrors> {
         None => "\n",
     };
 
-    let type_cache = TypeCache::new();
-    let expr = parser::parse_expr(&mut Symbols::new(), &type_cache, input)?;
-
     let source = Source::new(input);
     let arena = pretty::Arena::new();
     let printer = pretty_print::Printer::new(&arena, &source);
-    Ok(printer.format(100, newline, &expr))
+    printer.format(100, newline, &expr)
+}
+
+pub fn format_expr(input: &str) -> Result<String, parser::ParseErrors> {
+    let type_cache = TypeCache::new();
+    let expr = parser::parse_expr(&mut Symbols::new(), &type_cache, input)?;
+
+    Ok(pretty_expr(&expr, input))
 }
