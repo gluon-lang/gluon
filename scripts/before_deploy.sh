be93b55f32e5259a96e4aff60ffeb049748e0aa6
Markus Westerlind <marwes91@gmail.com>
2018-02-03 18:22:52 +0100

    feat: Deploy builds using trust and cross

---
+++ scripts/before_deploy.sh
@@ -0,0 +1,33 @@
+# This script takes care of building your crate and packaging it for release
+
+set -ex
+
+main() {
+    local src=$(pwd) \
+          stage=
+
+    case $TRAVIS_OS_NAME in
+        linux)
+            stage=$(mktemp -d)
+            ;;
+        osx)
+            stage=$(mktemp -d -t tmp)
+            ;;
+    esac
+
+    test -f Cargo.lock || cargo generate-lockfile
+
+    cross build -p gluon_repl --target $TARGET --release
+
+    # Copy the files that are needed in the distribution
+    cp target/$TARGET/release/gluon $stage/
+    cp -r std/ $stage/
+
+    cd $stage
+    tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
+    cd $src
+
+    rm -rf $stage
+}
+
+main
