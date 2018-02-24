c2b69435e34dcbbb83c13f7a37e2c89de55864ad
Markus Westerlind <marwes91@gmail.com>
2018-02-24 21:13:14 +0100

    chore: Forward arguments from travis.sh

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -3,9 +3,9 @@ set -ex
 
 export RUST_BACKTRACE=1
 
-cargo test --features test --all
-cargo check --benches --features test
-cargo check --all --no-default-features
+cargo test --features test --all "$@"
+cargo check --benches --features test "$@"
+cargo check --all --no-default-features "$@"
 
 echo "TRAVIS_RUST_VERSION=$TRAVIS_RUST_VERSION"
-[ "$TRAVIS_RUST_VERSION" != "nightly" ] || cargo test --features "test nightly" -p gluon compile_test
+[ "$TRAVIS_RUST_VERSION" != "nightly" ] || cargo test --features "test nightly" -p gluon compile_test "$@"
