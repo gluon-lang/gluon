119be37303f31fbedfd9341c8c5e9924ff5fac0d
memoryruins <memoryruinsmusic@gmail.com>
2018-03-13 23:40:57 -0400

    Update test macro for use of Result.

--- repl/tests/tests.rs
+++ repl/tests/tests.rs
@@ -4,6 +4,7 @@ extern crate pretty_assertions;
 extern crate rexpect;
 
 use rexpect::spawn;
+use rexpect::errors::*;
 
 use std::env;
 use std::fs::File;
@@ -71,113 +72,153 @@ static TIMEOUT: u64 = 10_000;
 static PROMPT: &str = "> ";
 
 macro_rules! test {
-    ($e: expr) => { $e.unwrap_or_else(|err| panic!("{}", err)) }
+    ($b: block) => {
+        || -> Result<()> { 
+            $b
+        }().unwrap_or_else(|err| panic!("{}", err));
+    };
 }
 
 #[test]
 fn prompt() {
-    let mut repl = test!(spawn(COMMAND, Some(TIMEOUT)));
-    test!(repl.exp_string(PROMPT));
+    test!({
+        let mut repl = spawn(COMMAND, Some(TIMEOUT))?;
+        repl.exp_string(PROMPT)?;
+        
+        Ok(())
+    });
 }
 
 #[test]
 fn exit() {
-    let mut repl = test!(spawn(COMMAND, Some(TIMEOUT)));
-    test!(repl.exp_string(PROMPT));
+    test!({
+        let mut repl = spawn(COMMAND, Some(TIMEOUT))?;
+        repl.exp_string(PROMPT)?;
 
-    test!(repl.send_line(":q"));
-    test!(repl.exp_eof());
+        repl.send_line(":q")?;
+        repl.exp_eof()?;
+        
+        Ok(())
+    });
 }
 
 
 #[test]
 #[ignore] // TODO fix intermittent test failure
 fn hello_world() {
-    let mut repl = test!(spawn(COMMAND, Some(TIMEOUT)));
-    test!(repl.exp_string(PROMPT));
+    test!({
+    let mut repl = spawn(COMMAND, Some(TIMEOUT))?;
+        repl.exp_string(PROMPT)?;
 
-    test!(repl.send_line("let io = import! std.io"));
+        repl.send_line("let io = import! std.io")?;
 
-    test!(repl.send_line("io.println \"Hello world\""));
-    test!(repl.exp_string("Hello world"));
+        repl.send_line("io.println \"Hello world\"")?;
+        repl.exp_string("Hello world")?;
+    
+        Ok(())
+    });
 }
 
 #[test]
 fn expression_types() {
-    let mut repl = test!(spawn(COMMAND, Some(TIMEOUT)));
-    test!(repl.exp_string(PROMPT));
+    test!({
+        let mut repl = spawn(COMMAND, Some(TIMEOUT))?;
+        repl.exp_string(PROMPT)?;
 
-    test!(repl.send_line(":t 5"));
-    test!(repl.exp_string("Int"));
+        repl.send_line(":t 5")?;
+        repl.exp_string("Int")?;
 
-    test!(repl.send_line(":t 5 + 5"));
-    test!(repl.exp_string("Int -> Int"));
+        repl.send_line(":t 5 + 5")?;
+        repl.exp_string("Int -> Int")?;
 
-    test!(repl.send_line(":t \"gluon\""));
-    test!(repl.exp_string("String"));
+        repl.send_line(":t \"gluon\"")?;
+        repl.exp_string("String")?;
+        
+        Ok(())
+    });
 }
 
 #[test]
 fn names() {
-    let mut repl = test!(spawn(COMMAND, Some(TIMEOUT)));
-    test!(repl.exp_string(PROMPT));
+    test!({
+        let mut repl = spawn(COMMAND, Some(TIMEOUT))?;
+        repl.exp_string(PROMPT)?;
 
-    test!(repl.send_line(":i std.prelude.show"));
-    test!(repl.exp_string("std.prelude.show: forall a . [std.prelude.Show a] -> a -> String"));
+        repl.send_line(":i std.prelude.show")?;
+        repl.exp_string("std.prelude.show: forall a . [std.prelude.Show a] -> a -> String")?;
+        
+        Ok(())
+    });
 }
 
 #[test]
 fn comments() {
-    let mut repl = test!(spawn(COMMAND, Some(TIMEOUT)));
-    test!(repl.exp_string(PROMPT));
+    test!({
+        let mut repl = spawn(COMMAND, Some(TIMEOUT))?;
+        repl.exp_string(PROMPT)?;
 
-    test!(repl.send_line("1 + 2 // Calls the + function on 1 and 2"));
-    test!(repl.exp_string("3"));
+        repl.send_line("1 + 2 // Calls the + function on 1 and 2")?;
+        repl.exp_string("3")?;
 
-    test!(repl.send_line("1 + 2 /* Calls the + function on 1 and 2 */"));
-    test!(repl.exp_string("3"));
+        repl.send_line("1 + 2 /* Calls the + function on 1 and 2 */")?;
+        repl.exp_string("3")?;
+    
+        Ok(())
+    });
 }
 
 #[test]
 fn if_expressions() {
-    let mut repl = test!(spawn(COMMAND, Some(TIMEOUT)));
-    test!(repl.exp_string(PROMPT));
+    test!({
+        let mut repl = spawn(COMMAND, Some(TIMEOUT))?;
+        repl.exp_string(PROMPT)?;
 
-    test!(repl.send_line("if True then 1 else 0"));
-    test!(repl.exp_string("1"));
+        repl.send_line("if True then 1 else 0")?;
+        repl.exp_string("1")?;
 
-    test!(repl.send_line("if False then 1 else 0"));
-    test!(repl.exp_string("0"));
+        repl.send_line("if False then 1 else 0")?;
+        repl.exp_string("0")?;
+        
+        Ok(())
+    });
 }
 
 #[test]
 fn records() {
-    let mut repl = test!(spawn(COMMAND, Some(TIMEOUT)));
-    test!(repl.exp_string(PROMPT));
+    test!({
+        let mut repl = spawn(COMMAND, Some(TIMEOUT))?;
+        repl.exp_string(PROMPT)?;
 
-    test!(repl.send_line("let record = { pi = 3.14, add1 = (+) 1.0 }"));
+        repl.send_line("let record = { pi = 3.14, add1 = (+) 1.0 }")?;
 
-    test!(repl.send_line("record.pi"));
-    test!(repl.exp_string("3.14"));
+        repl.send_line("record.pi")?;
+        repl.exp_string("3.14")?;
 
-    test!(repl.send_line("let record_2 = {x = 1 .. record }"));
+        repl.send_line("let record_2 = {x = 1 .. record }")?;
 
-    test!(repl.send_line("record_2.x"));
-    test!(repl.exp_string("1"));
+        repl.send_line("record_2.x")?;
+        repl.exp_string("1")?;
 
-    test!(repl.send_line("record_2.pi"));
-    test!(repl.exp_string("3.14"));
+        repl.send_line("record_2.pi")?;
+        repl.exp_string("3.14")?;
+    
+        Ok(())
+    });
 }
 
 #[test]
 fn arrays() {
-    let mut repl = test!(spawn(COMMAND, Some(TIMEOUT)));
-    test!(repl.exp_string(PROMPT));
+    test!({
+        let mut repl = spawn(COMMAND, Some(TIMEOUT))?;
+        repl.exp_string(PROMPT)?;
 
-    test!(repl.send_line("let array = import! std.array"));
+        repl.send_line("let array = import! std.array")?;
 
-    test!(repl.send_line("array.len [1, 2, 3]"));
-    test!(repl.exp_string("3"));
+        repl.send_line("array.len [1, 2, 3]")?;
+        repl.exp_string("3")?;
+        
+        Ok(())
+    });
 }
 
 
