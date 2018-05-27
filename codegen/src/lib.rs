58a6455f0097e35d8985ddf085901db34d295934
Robin Mirow <robinmirow@live.com>
2018-05-28 00:43:42 +0200

    Hide derive functions from docs

--- codegen/src/lib.rs
+++ codegen/src/lib.rs
@@ -140,21 +140,25 @@ mod shared;
 mod userdata;
 mod vm_type;
 
+#[doc(hidden)]
 #[proc_macro_derive(Getable)]
 pub fn getable(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     getable::derive(input.into()).into()
 }
 
+#[doc(hidden)]
 #[proc_macro_derive(Pushable)]
 pub fn pushable(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     pushable::derive(input.into()).into()
 }
 
+#[doc(hidden)]
 #[proc_macro_derive(Userdata)]
 pub fn userdata(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     userdata::derive(input.into()).into()
 }
 
+#[doc(hidden)]
 #[proc_macro_derive(VmType, attributes(gluon))]
 pub fn vm_type(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     vm_type::derive(input.into()).into()
