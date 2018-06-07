gluon-lang/gluon

commit 98406e238a14f4173bcc76e45f3ad417e967be72

docs: Don't display generated implicit arguments in docs

check/src/metadata.rs
@@ -36,6 +36,8 @@ pub fn metadata(
                     metadata.args = bind
                         .args
                         .iter()
+                        // Ignore generated arguments
+                        .filter(|arg| !arg.name.value.name.definition_name().starts_with('#'))
                         .map(|arg| Argument {
                             name: arg.name.value.name.clone(),
                             arg_type: arg.arg_type,

check/src/typecheck.rs
@@ -1309,7 +1309,7 @@ impl<'a> Typecheck<'a> {
                                 &mut args[i - 1].name.value
                             }
                             _ => {
-                                let id = Symbol::from(format!("implicit_arg"));
+                                let id = Symbol::from(format!("#implicit_arg"));
                                 let pos = if i == 0 {
                                     before_args_pos
                                 } else {

commit 5047f4413e152bf4235a69150f190c1f0cef9be0

Add a test and fix a regression in 98406e23

check/src/metadata.rs
@@ -37,7 +37,7 @@ pub fn metadata(
                         .args
                         .iter()
                         // Ignore generated arguments
-                        .filter(|arg| !arg.name.value.name.definition_name().starts_with('#'))
+                        .filter(|arg| !arg.name.value.name.definition_name().starts_with("__"))
                         .map(|arg| Argument {
                             name: arg.name.value.name.clone(),
                             arg_type: arg.arg_type,

check/src/typecheck.rs
@@ -1309,7 +1309,7 @@ impl<'a> Typecheck<'a> {
                                 &mut args[i - 1].name.value
                             }
                             _ => {
-                                let id = Symbol::from(format!("#implicit_arg"));
+                                let id = Symbol::from(format!("__implicit_arg"));
                                 let pos = if i == 0 {
                                     before_args_pos
                                 } else {
@@ -1329,7 +1329,7 @@ impl<'a> Typecheck<'a> {
                                     )),
                                 );
                                 i += 1;
-                                &mut args.last_mut().unwrap().name.value
+                                &mut args[i - 1].name.value
                             }
                         };
                         arg.typ = arg_type.clone();

check/tests/implicits.rs
@@ -68,7 +68,7 @@ f 42
 
     assert_eq!(result, Ok(Type::int()));
     assert_eq!(
-        r#"let f ?implicit_arg y : [Int] -> Int -> Int = y
+        r#"let f ?__implicit_arg y : [Int] -> Int -> Int = y
 #[implicit]
 let i = 123
 f ?i 42"#,
@@ -721,4 +721,57 @@ f Nil
             ..
         })
     );
+}
+
+#[test]
+fn compare() {
+    let _ = ::env_logger::try_init();
+    let text = r#"
+type Ordering = | LT | EQ | GT
+#[implicit]
+type Ord a = { compare : a -> a -> Ordering }
+
+let compare ?ord : [Ord a] -> a -> a -> Ordering = ord.compare
+
+let (<) l r : [Ord a] -> a -> a -> Bool =
+    match compare l r with
+    | LT -> True
+    | EQ -> False
+    | GT -> False
+()
+"#;
+    let (expr, _result) = support::typecheck_expr(text);
+
+    struct Visitor {
+        text: &'static str,
+        done: bool,
+    }
+    impl<'a> base::ast::Visitor<'a> for Visitor {
+        type Ident = Symbol;
+
+        fn visit_expr(&mut self, expr: &'a SpannedExpr<Symbol>) {
+            match expr.value {
+                ast::Expr::App {
+                    ref func,
+                    ref implicit_args,
+                    ..
+                } => {
+                    if let ast::Expr::Ident(ref id) = func.value {
+                        if id.name.definition_name() == "compare" {
+                            assert_eq!(
+                                "__implicit_arg",
+                                format::pretty_expr(self.text, &implicit_args[0]).trim()
+                            );
+                            self.done = true;
+                        }
+                    }
+                }
+                _ => (),
+            }
+            base::ast::walk_expr(self, expr)
+        }
+    }
+    let mut visitor = Visitor { text, done: false };
+    visitor.visit_expr(&expr);
+    assert!(visitor.done);
 }