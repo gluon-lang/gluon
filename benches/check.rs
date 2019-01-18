2a28dbe8e1d89fd23c1e55602e2d80ef22fa919a
Markus Westerlind <marwes91@gmail.com>
2019-01-18 22:01:03 +0100

    Benchmark 24 compilation
    Gives a bit more a realistic module

--- benches/check.rs
+++ benches/check.rs
@@ -6,8 +6,7 @@ extern crate gluon_base as base;
 extern crate gluon_check as check;
 extern crate gluon_parser as parser;
 
-use std::fs::File;
-use std::io::Read;
+use std::fs;
 
 use bencher::{black_box, Bencher};
 
@@ -18,11 +17,7 @@ fn typecheck_prelude(b: &mut Bencher) {
     let vm = new_vm();
     let mut compiler = Compiler::new();
     let MacroValue { expr } = {
-        let mut text = String::new();
-        File::open("std/prelude.glu")
-            .unwrap()
-            .read_to_string(&mut text)
-            .unwrap();
+        let text = fs::read_to_string("std/prelude.glu").unwrap();
         text.expand_macro(&mut compiler, &vm, "std.prelude", &text)
             .unwrap_or_else(|(_, err)| panic!("{}", err))
     };
@@ -40,16 +35,30 @@ fn clone_prelude(b: &mut Bencher) {
     let vm = new_vm();
     let mut compiler = Compiler::new();
     let TypecheckValue { expr, .. } = {
-        let mut text = String::new();
-        File::open("std/prelude.glu")
-            .unwrap()
-            .read_to_string(&mut text)
-            .unwrap();
+        let text = fs::read_to_string("std/prelude.glu").unwrap();
         text.typecheck(&mut compiler, &vm, "std.prelude", &text)
             .unwrap_or_else(|err| panic!("{}", err))
     };
     b.iter(|| black_box(expr.clone()))
 }
 
-benchmark_group!(check, typecheck_prelude, clone_prelude);
+fn typecheck_24(b: &mut Bencher) {
+    let vm = new_vm();
+    let mut compiler = Compiler::new();
+    let MacroValue { expr } = {
+        let text = fs::read_to_string("examples/24.glu").unwrap();
+        text.expand_macro(&mut compiler, &vm, "examples.24", &text)
+            .unwrap_or_else(|(_, err)| panic!("{}", err))
+    };
+    b.iter(|| {
+        let result = MacroValue { expr: expr.clone() }.typecheck(&mut compiler, &vm, "<top>", "");
+        if let Err(ref err) = result {
+            println!("{}", err);
+            assert!(false);
+        }
+        black_box(result)
+    })
+}
+
+benchmark_group!(check, typecheck_prelude, clone_prelude, typecheck_24);
 benchmark_main!(check);
