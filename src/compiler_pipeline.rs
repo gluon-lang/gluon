99f854f969e96f3d524b6bdb1f307d9652c4961d
Markus Westerlind <marwes91@gmail.com>
2019-01-27 20:33:02 +0100

    refactor: Simplify compiler_pipeline

--- src/compiler_pipeline.rs
+++ src/compiler_pipeline.rs
@@ -227,25 +227,18 @@ where
         file: &str,
         expr_str: &str,
     ) -> SalvageResult<Renamed<Self::Expr>> {
-        let mut macro_error = None;
-        let expr = match self.expand_macro(compiler, thread, file, expr_str) {
-            Ok(expr) => expr,
-            Err((Some(expr), err)) => {
-                macro_error = Some(err);
-                expr
-            }
-            Err((None, err)) => return Err((None, err)),
-        };
-        match expr.rename(compiler, thread, file, expr_str) {
-            Ok(value) => match macro_error {
-                Some(err) => return Err((Some(value), err)),
-                None => Ok(value),
+        join_result(
+            self.expand_macro(compiler, thread, file, expr_str),
+            |MacroValue { expr }| {
+                MacroValue {
+                    expr: expr.borrow_mut(),
+                }
+                .rename(compiler, thread, file, expr_str)
+                .map(|_| ())
+                .map_err(|(opt, err)| (opt.map(|_| ()), err))
             },
-            Err((opt, err)) => Err((
-                opt,
-                Errors::from(macro_error.into_iter().chain(Some(err)).collect::<Vec<_>>()).into(),
-            )),
-        }
+            |MacroValue { expr }| Renamed { expr },
+        )
     }
 }
 
