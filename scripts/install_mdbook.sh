6d9e9457064dbfd3d7cccf11d9f4091103a634f3
Markus Westerlind <marwes91@gmail.com>
2019-01-27 22:08:26 +0100

    Fix install scripts

--- scripts/install_mdbook.sh
+++ scripts/install_mdbook.sh
@@ -5,4 +5,4 @@ set -ex
 MDBOOK_VERSION='mdbook-v0.2.1-x86_64-unknown-linux-musl'
 curl -L "https://github.com/rust-lang-nursery/mdBook/releases/download/v0.2.1/$MDBOOK_VERSION.tar.gz" | tar -xvz
 chmod +x ./mdbook
-mv ./mdbook /usr/local/bin/ 
+mv ./mdbook $HOME/bin/ 
