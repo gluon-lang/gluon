93111515751c0b554f921d1d6923bd7d69dfae4f
Markus Westerlind <marwes91@gmail.com>
2019-01-27 19:37:04 +0100

    chore: Install binaries directly instead of through cargo

---
+++ scripts/install_sccache.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -ex
+
+SCCACHE_VERSION='sccache-0.2.8-x86_64-unknown-linux-musl'
+curl -L "https://github.com/mozilla/sccache/releases/download/0.2.8/$SCCACHE_VERSION.tar.gz" | tar -xvz
+mv $SCCACHE_VERSION/sccache .
+chmod +x ./sccache
+mv ./sccache /usr/local/bin/ 
