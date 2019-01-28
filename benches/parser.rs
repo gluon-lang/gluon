a21771748adca97b0ce2b5a822459c25227538a6
Markus Westerlind <marwes91@gmail.com>
2019-01-28 23:07:49 +0100

    chore: Add more parser benchmarks

--- benches/parser.rs
+++ benches/parser.rs
@@ -4,19 +4,18 @@ extern crate criterion;
 extern crate gluon_base as base;
 extern crate gluon_parser as parser;
 
+use std::fs;
+
 use criterion::{Bencher, Criterion};
 
-use crate::base::symbol::{SymbolModule, Symbols};
-use crate::base::types::TypeCache;
+use crate::base::{
+    symbol::{SymbolModule, Symbols},
+    types::TypeCache,
+};
 
-fn parse_prelude(b: &mut Bencher) {
-    use std::fs::File;
-    use std::io::Read;
-    let mut text = String::new();
-    File::open("std/prelude.glu")
-        .unwrap()
-        .read_to_string(&mut text)
-        .unwrap();
+fn parse_file(b: &mut Bencher, file: &str) {
+    let text = fs::read_to_string(file).unwrap();
+
     b.iter(|| {
         let mut symbols = Symbols::new();
         let mut symbols = SymbolModule::new("".into(), &mut symbols);
@@ -26,7 +25,11 @@ fn parse_prelude(b: &mut Bencher) {
 }
 
 fn parse_benchmark(c: &mut Criterion) {
-    c.bench_function("std/prelude", parse_prelude);
+    c.bench_function("std/prelude", |b| parse_file(b, "std/prelude.glu"));
+    c.bench_function("examples/lisp", |b| parse_file(b, "examples/lisp/lisp.glu"));
+    c.bench_function("examples/http", |b| {
+        parse_file(b, "examples/http/server.glu")
+    });
 }
 
 criterion_group!(parser, parse_benchmark);
