98f6731f3990ae99151d8d34f1f8f85523a05cbc
Markus Westerlind <marwes91@gmail.com>
2019-02-11 22:11:43 +0100

    Version 0.11.0

--- vm/src/lib.rs
+++ vm/src/lib.rs
@@ -1,5 +1,5 @@
 //! Crate which contain the virtual machine which executes gluon programs
-#![doc(html_root_url = "https://docs.rs/gluon_vm/0.10.1")] // # GLUON
+#![doc(html_root_url = "https://docs.rs/gluon_vm/0.11.0")] // # GLUON
 #![recursion_limit = "1024"]
 
 #[macro_use]
