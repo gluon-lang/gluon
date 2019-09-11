04b4bf759df2125de1488ea12ea24e6318fd85fc
Etherian <etherain@gmail.com>
2019-09-11 19:32:01 -0400

    add rest of std::time functions

--- src/std_lib/time.rs
+++ src/std_lib/time.rs
@@ -13,6 +13,23 @@ use crate::vm::{
     ExternModule,
 };
 
+const NANOS_PER_SEC: u64 = 1_000_000_000;
+
+macro_rules! try_convert {
+    ($val: expr, $msg: expr) => {{
+        $val.try_into().map_err(|_| $msg.to_string()).into()
+    }};
+}
+
+macro_rules! convert_or_abort {
+    ($val: expr, $msg: expr) => {{
+        match try_convert!($val, $msg) {
+            RuntimeResult::Return(x) => x,
+            RuntimeResult::Panic(e) => return RuntimeResult::Panic(e),
+        }
+    }};
+}
+
 mod duration {
     use super::*;
 
@@ -23,16 +40,89 @@ mod duration {
     #[gluon_trace(skip)]
     pub(crate) struct Duration(pub(crate) time::Duration);
 
+    pub(crate) fn new(secs: VmInt, nanos: VmInt) -> RuntimeResult<Duration, String> {
+        let secs: u64 = convert_or_abort!(secs, "seconds must be non-negative");
+        let nanos: u64 = convert_or_abort!(nanos, "nanoseconds must be non-negative");
+        let secs = secs.checked_add(nanos / NANOS_PER_SEC);
+        let nanos = (nanos % NANOS_PER_SEC) as u32;
+
+        match secs {
+            Some(secs) => RuntimeResult::Return(Duration(time::Duration::new(secs, nanos))),
+            // should never occur because i64.max_value() + i64.max_value() / 1e9 < u64.max_value()
+            None => RuntimeResult::Panic("seconds overflowed when creating a new Duration".to_string()),
+        }
+    }
+
+    pub(crate) fn from_secs(secs: VmInt) -> RuntimeResult<Duration, String> {
+        new(secs, 0)
+    }
+
+    pub(crate) fn from_millis(millis: VmInt) -> RuntimeResult<Duration, String> {
+        let millis: u64 = convert_or_abort!(millis, "milliseconds must be non-negative");
+        RuntimeResult::Return(Duration(time::Duration::from_millis(millis)))
+    }
+
+    pub(crate) fn from_micros(micros: VmInt) -> RuntimeResult<Duration, String> {
+        let micros: u64 = convert_or_abort!(micros, "microseconds must be non-negative");
+        RuntimeResult::Return(Duration(time::Duration::from_micros(micros)))
+    }
+
+    pub(crate) fn from_nanos(nanos: VmInt) -> RuntimeResult<Duration, String> {
+        new(0, nanos)
+    }
+
     pub(crate) fn as_secs(dur: &Duration) -> RuntimeResult<VmInt, String> {
-        dur.0.as_secs()
-            .try_into()
-            .map_err(|_| String::from("Overflow when trying to convert Duration into seconds"))
-            .into()
+        try_convert!(dur.0.as_secs(), "Overflow when trying to convert Duration into seconds")
+    }
+
+    pub(crate) fn subsec_millis(dur: &Duration) -> VmInt {
+        dur.0.subsec_millis() as VmInt
+    }
+
+    pub(crate) fn subsec_micros(dur: &Duration) -> VmInt {
+        dur.0.subsec_micros() as VmInt
     }
 
     pub(crate) fn subsec_nanos(dur: &Duration) -> VmInt {
         dur.0.subsec_nanos() as VmInt
     }
+
+    pub(crate) fn as_millis(dur: &Duration) -> RuntimeResult<VmInt, String> {
+        try_convert!(dur.0.as_millis(), "Overflow when trying to convert Duration into milliseconds")
+    }
+
+    pub(crate) fn as_micros(dur: &Duration) -> RuntimeResult<VmInt, String> {
+        try_convert!(dur.0.as_micros(), "Overflow when trying to convert Duration into microseconds")
+    }
+
+    pub(crate) fn as_nanos(dur: &Duration) -> RuntimeResult<VmInt, String> {
+        try_convert!(dur.0.as_nanos(), "Overflow when trying to convert Duration into nanoseconds")
+    }
+
+    pub(crate) fn checked_add(dur1: &Duration, dur2: &Duration) -> Option<Duration> {
+        dur1.0.checked_add(dur2.0).map(Duration)
+    }
+
+    pub(crate) fn checked_sub(dur1: &Duration, dur2: &Duration) -> Option<Duration> {
+        dur1.0.checked_sub(dur2.0).map(Duration)
+    }
+
+    // should this be Option because it better matches Rust's API or Result because it could fail
+    // in two different ways?
+    pub(crate) fn checked_mul(dur1: &Duration, multiplier: VmInt) -> Option<Duration> {
+        match multiplier.try_into() {
+            Ok(n) => dur1.0.checked_mul(n).map(Duration),
+            Err(_) => None,
+        }
+    }
+
+    pub(crate) fn checked_div(dur1: &Duration, divisor: VmInt) -> Option<Duration> {
+        match divisor.try_into() {
+            Ok(n) => dur1.0.checked_div(n).map(Duration),
+            Err(_) => None,
+        }
+    }
+
 }
 
 mod instant {
@@ -64,6 +154,14 @@ mod instant {
     pub(crate) fn elapsed(earlier: &Instant) -> Duration {
         Duration(earlier.0.elapsed())
     }
+
+    pub(crate) fn checked_add(moment: &Instant, dur: &Duration) -> Option<Instant> {
+        moment.0.checked_add(dur.0).map(Instant)
+    }
+
+    pub(crate) fn checked_sub(moment: &Instant, dur: &Duration) -> Option<Instant> {
+        moment.0.checked_sub(dur.0).map(Instant)
+    }
 }
 
 mod system_time {
@@ -93,6 +191,14 @@ mod system_time {
             .map(|x| Duration(x))
             .map_err(|e| Duration(e.duration()))
     }
+
+    pub(crate) fn checked_add(moment: &SystemTime, dur: &Duration) -> Option<SystemTime> {
+        moment.0.checked_add(dur.0).map(SystemTime)
+    }
+
+    pub(crate) fn checked_sub(moment: &SystemTime, dur: &Duration) -> Option<SystemTime> {
+        moment.0.checked_sub(dur.0).map(SystemTime)
+    }
 }
 
 mod std {
@@ -112,8 +218,24 @@ pub fn load(vm: &Thread) -> vm::Result<ExternModule> {
             duration => record! {
                 type std::time::Duration => duration::Duration,
 
+                new => primitive!(2, std::time::prim::duration::new),
+                from_secs => primitive!(1, std::time::prim::duration::from_secs),
+                from_millis => primitive!(1, std::time::prim::duration::from_millis),
+                from_micros => primitive!(1, std::time::prim::duration::from_micros),
+                from_nanos => primitive!(1, std::time::prim::duration::from_nanos),
+
                 as_secs => primitive!(1, std::time::prim::duration::as_secs),
+                subsec_millis => primitive!(1, std::time::prim::duration::subsec_millis),
+                subsec_micros => primitive!(1, std::time::prim::duration::subsec_micros),
                 subsec_nanos => primitive!(1, std::time::prim::duration::subsec_nanos),
+                as_millis => primitive!(1, std::time::prim::duration::as_millis),
+                as_micros => primitive!(1, std::time::prim::duration::as_micros),
+                as_nanos => primitive!(1, std::time::prim::duration::as_nanos),
+
+                checked_add => primitive!(2, std::time::prim::duration::checked_add),
+                checked_sub => primitive!(2, std::time::prim::duration::checked_sub),
+                checked_mul => primitive!(2, std::time::prim::duration::checked_mul),
+                checked_div => primitive!(2, std::time::prim::duration::checked_div),
             },
             instant => record! {
                 type std::time::Instant => instant::Instant,
@@ -121,6 +243,9 @@ pub fn load(vm: &Thread) -> vm::Result<ExternModule> {
                 now => primitive!(1, std::time::prim::instant::now),
                 duration_since => primitive!(2, std::time::prim::instant::duration_since),
                 elapsed => primitive!(1, std::time::prim::instant::elapsed),
+
+                checked_add => primitive!(2, std::time::prim::instant::checked_add),
+                checked_sub => primitive!(2, std::time::prim::instant::checked_sub),
             },
             system_time => record! {
                 type std::time::SystemTime => system_time::SystemTime,
@@ -130,6 +255,9 @@ pub fn load(vm: &Thread) -> vm::Result<ExternModule> {
                 now => primitive!(1, std::time::prim::system_time::now),
                 duration_since => primitive!(2, std::time::prim::system_time::duration_since),
                 elapsed => primitive!(1, std::time::prim::system_time::elapsed),
+
+                checked_add => primitive!(2, std::time::prim::system_time::checked_add),
+                checked_sub => primitive!(2, std::time::prim::system_time::checked_sub),
             },
         }
     )
