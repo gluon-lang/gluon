4aa9a738c980e7a8ac99a0893182753318d57f5b
Markus Westerlind <marwes91@gmail.com>
2019-10-05 09:50:03 +0200

    Run --no-default-features after compiletest so  the correct artifacts are used

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -10,8 +10,9 @@ if [ -z $NO_NORMAL_TEST ]; then
     cargo test --features "test" --benches "$@" -- --test
     echo "" | cargo run --features "test" --example 24
     cargo run --features "test" --example marshalling
-    cargo check --all --no-default-features "$@"
 
     echo "TRAVIS_RUST_VERSION=$TRAVIS_RUST_VERSION"
     (echo $TRAVIS_RUST_VERSION | grep nightly) && cargo test --features "test nightly" -p gluon --test compiletest "$@"
+
+    cargo check --all --no-default-features "$@"
 fi
