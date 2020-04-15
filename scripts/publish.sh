cb43d5327f3c1759d2795116fc8f4dbee7a1bce6
Markus Westerlind <markus.westerlind@distilnetworks.com>
2020-04-15 15:39:34 +0200

    Make the publish script more robust

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
-set -ex
+
+VERSION=$(echo $1 | sed 's/v//')
+shift
 
 declare -a PROJECTS=(
-    codegen
-    base
-    parser
-    check
-    completion
-    vm
-    format
-    .
-    c-api
-    doc
-    repl
+    gluon
+    gluon_c-api
+    gluon_doc
+    gluon_repl
 )
 
 for PROJECT in "${PROJECTS[@]}"
 do
-    sleep 4
-    (cd $PROJECT && cargo publish $@)
+    PROJECT_PATH=$(echo "$PROJECT" | sed 's/gluon_//' | sed 's/gluon/./')
+    echo $PROJECT_PATH
+    (cd "${PROJECT_PATH}" && cargo publish "$@")
+    if [[ $? ]]; then
+        exit 1
+    fi
+    echo "Waiting for ${PROJECT} to publish"
+    N=0
+    while ! cargo search "${PROJECT}" | head -1 | grep "${VERSION}"; do
+        printf '.'
+        sleep 3
+        N=$((N+1))
+        if [[ $N -gt 10 ]]; then
+            echo "${PROJECT} did not get published!"
+            exit 1
+        fi
+    done
 done
