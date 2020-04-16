ed4f78fa3ad92d2f37c95542fa9618be65032277
Markus Westerlind <marwes91@gmail.com>
2020-04-16 21:58:40 +0200

    Fix release script again

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,14 +4,6 @@ VERSION=$(echo $1 | sed 's/v//')
 shift
 
 declare -a PROJECTS=(
-    gluon_codegen
-    gluon_base
-    gluon_parser
-    gluon_check
-    gluon_completion
-    gluon_vm
-    gluon_format
-    gluon
     gluon_c-api
     gluon_doc
     gluon_repl
@@ -20,20 +12,10 @@ declare -a PROJECTS=(
 for PROJECT in "${PROJECTS[@]}"
 do
     PROJECT_PATH=$(echo "$PROJECT" | sed 's/gluon_//' | sed 's/gluon/./')
-    echo $PROJECT_PATH
 
-    if cd "${PROJECT_PATH}" && cargo publish "$@"; then
+    if ! (cd "${PROJECT_PATH}" && cargo publish "$@"); then
         exit 1
     fi
     echo "Waiting for ${PROJECT} to publish"
-    N=0
-    while ! cargo search "${PROJECT}" | head -1 | grep "${VERSION}"; do
-        printf '.'
-        sleep 3
-        N=$((N+1))
-        if [[ $N -gt 10 ]]; then
-            echo "${PROJECT} did not get published!"
-            exit 1
-        fi
-    done
+    sleep 15
 done
