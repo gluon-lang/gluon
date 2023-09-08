7bfcbfe70a9129a1411ffd383dffd66ea51868bc
rowanfr <beanfun1@gmail.com>
2023-09-08 04:50:40 -0500

    Trying to do a basic log of the toolchains installed via CI

--- scripts/install_cross.sh
+++ scripts/install_cross.sh
@@ -3,6 +3,9 @@ set -ex
 main() {
     # This is necessary as shown in https://github.com/rust-lang/rust/issues/61925
     export RUSTC_WRAPPER=sccache
+    # Check sscache version
+    ls -a /home/travis/.rustup/toolchains/*
+    sccache --version
     # At this point cargo should be installed
     cargo install cross
 }
