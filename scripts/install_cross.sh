dbef05ed9ca0896338121c6314587f348d5e108c
rowanfr <beanfun1@gmail.com>
2023-09-08 00:13:14 -0500

    Alternate install for cross

--- scripts/install_cross.sh
+++ scripts/install_cross.sh
@@ -1,20 +1,8 @@
 set -ex
 
 main() {
-    local target=x86_64-unknown-linux-musl
-
-    # This fetches latest stable release
-    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
-                       | cut -d/ -f3 \
-                       | grep -E '^v[0.1.0-9.]+$' \
-                       | sort --version-sort \
-                       | tail -n1)
-    curl -LSfs https://japaric.github.io/trust/install.sh | \
-        sh -s -- \
-           --force \
-           --git japaric/cross \
-           --tag $tag \
-           --target $target
+    # At this point cargo should be installed
+    cargo install cross
 }
 
 main
