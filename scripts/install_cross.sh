7c2c68feddaf164cbce02646ff1a401a064da607
rowanfr <beanfun1@gmail.com>
2023-09-08 04:59:54 -0500

    Further attempt at debugging

--- scripts/install_cross.sh
+++ scripts/install_cross.sh
@@ -4,7 +4,7 @@ main() {
     # This is necessary as shown in https://github.com/rust-lang/rust/issues/61925
     export RUSTC_WRAPPER=sccache
     # Check sscache version
-    ls -a /home/travis/.rustup/toolchains/*
+    ls -a /home/travis/.rustup/toolchains/*/bin/*
     sccache --version
     # At this point cargo should be installed
     cargo install cross
