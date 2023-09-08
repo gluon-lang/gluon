05436805b0c1c43ecdd97e5367da79fea7465b84
rowanfr <beanfun1@gmail.com>
2023-09-08 00:21:43 -0500

    Added the first backslash for /tmp

--- scripts/install_cross.sh
+++ scripts/install_cross.sh
@@ -2,7 +2,7 @@ set -ex
 
 main() {
     # clean tmp of any stored artifacts
-    rm -r tmp/*
+    rm -r /tmp/*
     # clean cargo of any stored artifacts
     cargo clean
     # At this point cargo should be installed
