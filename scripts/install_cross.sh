cc3d8da0bc770b83bb3715a196dd30c26a89909c
rowanfr <beanfun1@gmail.com>
2023-09-08 00:21:06 -0500

    Clear tmp file recursively
    Clear tmp file recursively to prevent cargo artifacts from interfering

--- scripts/install_cross.sh
+++ scripts/install_cross.sh
@@ -1,6 +1,8 @@
 set -ex
 
 main() {
+    # clean tmp of any stored artifacts
+    rm -r tmp/*
     # clean cargo of any stored artifacts
     cargo clean
     # At this point cargo should be installed
