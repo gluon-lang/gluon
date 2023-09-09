01376cb225958af2e7456f15567a430630b14ba5
rowanfr <beanfun1@gmail.com>
2023-09-09 02:46:24 -0500

    Trying to understand error

--- scripts/install_mdbook.sh
+++ scripts/install_mdbook.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
+#Getting issue error: unrecognized subcommand '/home/travis/.rustup/toolchains/nightly-2023-09-05-x86_64-unknown-linux-gnu/bin/rustc'
+ls /home/travis/.rustup/toolchains/nightly-2023-09-05-x86_64-unknown-linux-gnu/bin/*
+/home/travis/.rustup/toolchains/nightly-2023-09-05-x86_64-unknown-linux-gnu/bin/rustc --version
+
 if [[ $1 == *"apple"* ]]; then
     exit 0
 else
