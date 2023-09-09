1f40c2c0a911ef2527d635a92fbb443690d289e5
rowanfr <beanfun1@gmail.com>
2023-09-09 01:07:14 -0500

    Revert to download of precompiled binaries for cross

--- scripts/install_cross.sh
+++ scripts/install_cross.sh
@@ -1,13 +1,24 @@
 set -ex
 
 main() {
-    # This is necessary as shown in https://github.com/rust-lang/rust/issues/61925
-    export RUSTC_WRAPPER=sccache
-    # Check sscache version
-    ls -a /home/travis/.rustup/toolchains/*/bin/*
-    sccache --version
-    # At this point cargo should be installed
-    cargo install cross
+    # Sample link
+    # https://github.com/cross-rs/cross/releases/download/v0.2.5/cross-x86_64-unknown-linux-musl.tar.gz
+
+    VERSION="v0.2.5"
+
+    CROSS_VERSION='cross-x86_64-unknown-linux-musl'
+
+    pushd ${WORK_DIR}
+
+    curl -L "https://github.com/cross-rs/cross/releases/download/${VERSION}/$CROSS_VERSION.tar.gz" | tar -xvz
+    mv $CROSS_VERSION/cross .
+    mv $CROSS_VERSION/cross-util .
+    
+    # it is sccache-dist on linux and sccache on mac
+    chmod +x ./cross
+    mv ./cross $HOME/bin/
+    chmod +x ./cross-util
+    mv ./cross-util $HOME/bin/
 }
 
 main
