cb3e40275898565b074b1b11d9f1c46e8b7687a6
rowanfr <beanfun1@gmail.com>
2023-09-09 01:19:48 -0500

    Placed cross install before sccache

--- scripts/install_cross.sh
+++ scripts/install_cross.sh
@@ -1,22 +1,7 @@
 set -ex
 
 main() {
-    # Sample link
-    # https://github.com/cross-rs/cross/releases/download/v0.2.5/cross-x86_64-unknown-linux-musl.tar.gz
-
-    VERSION="v0.2.5"
-
-    CROSS_VERSION='cross-x86_64-unknown-linux-musl'
-
-    curl -L "https://github.com/cross-rs/cross/releases/download/${VERSION}/$CROSS_VERSION.tar.gz" | tar -xvz
-    mv $CROSS_VERSION/cross .
-    mv $CROSS_VERSION/cross-util .
-    
-    # it is sccache-dist on linux and sccache on mac
-    chmod +x ./cross
-    mv ./cross $HOME/bin/
-    chmod +x ./cross-util
-    mv ./cross-util $HOME/bin/
+    cargo install cross --git https://github.com/cross-rs/cross
 }
 
 main
