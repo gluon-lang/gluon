56d7fb10da71bcfceda25b083ad91b9e6d741a34
rowanfr <beanfun1@gmail.com>
2023-09-09 01:23:51 -0500

    Reverted changes

--- scripts/install_cross.sh
+++ scripts/install_cross.sh
@@ -1,7 +1,20 @@
 set -ex
 
 main() {
-    cargo install cross --git https://github.com/cross-rs/cross
+    local target=x86_64-unknown-linux-musl
+
+    # This fetches latest stable release
+    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
+                       | cut -d/ -f3 \
+                       | grep -E '^v[0.1.0-9.]+$' \
+                       | sort --version-sort \
+                       | tail -n1)
+    curl -LSfs https://japaric.github.io/trust/install.sh | \
+        sh -s -- \
+           --force \
+           --git japaric/cross \
+           --tag $tag \
+           --target $target
 }
 
 main
